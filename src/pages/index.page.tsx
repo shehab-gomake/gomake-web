3c95d5ea8b14d763fe9fbdbab926a8a8fac71b92
faraj@gomake.net <faraj@gomake.net>
2023-03-19 13:23:13 +0200

    remove unused variable

--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -1,7 +1,4 @@
-import { useGomakeAuth, useGomakeRouter } from "@/hooks";
 import { CustomerAuthLayout } from "@/layouts";
-import { Outfit } from "next/font/google";
-import { useEffect } from "react";
 
 export default function Home() {
   return (
