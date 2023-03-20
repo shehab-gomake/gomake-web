86ab5e35bff7b61c3df2d87a1f259b5f0d78c5c2
riadFadila <riad@gomake.net>
2023-03-20 11:40:11 +0200

    fix after merge

--- src/store/index.ts
+++ src/store/index.ts
@@ -3,6 +3,4 @@ export * from "./theme";
 export * from "./user";
 export * from "./admin";
 export * from "./permissions";
-
-export * from './dashboard-date'
-export * from './dashboard-date'
+export * from './dashboard-date';
