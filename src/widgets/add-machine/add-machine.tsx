21afe7ebf4ff461786b51e10721e8b0cf81ed307
riadFadila <riad@gomake.net>
2023-03-23 10:59:45 +0200

    ---wip

---
+++ src/widgets/add-machine/add-machine.tsx
@@ -0,0 +1,11 @@
+import {useStyle} from "@/widgets/add-machine/style";
+
+const AddMachine = () => {
+    const {classes} = useStyle();
+    return (
+        <div>
+            <h2>Add printing machine</h2>
+        </div>
+    );
+}
+export {AddMachine}
