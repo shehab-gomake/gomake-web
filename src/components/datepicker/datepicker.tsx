40fec00976e4c3f6396de9d416f13694ace99f74
riadFadila <riad@gomake.net>
2023-03-31 00:08:27 +0300

    add close button to datepicker

--- src/components/datepicker/datepicker.tsx
+++ src/components/datepicker/datepicker.tsx
@@ -1,4 +1,4 @@
-import {Modal} from "@mui/material";
+import {IconButton, Modal} from "@mui/material";
 import {DateRangePicker} from "react-date-range";
 import {GomakePrimaryButton} from "@/components";
 import {useStyle} from "@/components/datepicker/style";
@@ -11,6 +11,7 @@ import AddBoxOutlinedIcon from '@mui/icons-material/AddBoxOutlined';
 import {TODAY_DATE_RANGE} from "@/shared/constant";
 import {useGomakeDateRange} from "@/hooks";
 import {useTranslation} from "react-i18next";
+import {CloseIcon} from "next/dist/client/components/react-dev-overlay/internal/icons/CloseIcon";
 
 const GoMakeDatepicker = ({}: IGoMakeDatepicker) => {
     const {t} = useTranslation();
@@ -38,6 +39,7 @@ const GoMakeDatepicker = ({}: IGoMakeDatepicker) => {
                 aria-labelledby="modal-modal-title"
                 aria-describedby="modal-modal-description">
                 <div style={classes.datepickerContainer}>
+                    <div><IconButton onClick={() => setOpenDatepicker(false)}><CloseIcon/></IconButton></div>
                     <DateRangePicker
                         onChange={item => setState({...state, ...item})}
                         months={1}
