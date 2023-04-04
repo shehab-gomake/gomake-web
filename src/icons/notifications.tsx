75719bf465fd87bf9b4910b04f43a0099b077c60
Ahmed <shehab@gomake.net>
2023-04-04 15:06:32 +0300

    fix bugs for svg icons

--- src/icons/notifications.tsx
+++ src/icons/notifications.tsx
@@ -11,22 +11,22 @@ const Notifications = () => {
       <path
         d="M20 15.44V18.77"
         stroke="white"
-        stroke-width="1.5"
-        stroke-miterlimit="10"
-        stroke-linecap="round"
+        strokeWidth="1.5"
+        strokeMiterlimit="10"
+        strokeLinecap="round"
       />
       <path
         d="M20.0199 11C16.3399 11 13.3599 13.98 13.3599 17.66V19.76C13.3599 20.44 13.0799 21.46 12.7299 22.04L11.4599 24.16C10.6799 25.47 11.2199 26.93 12.6599 27.41C17.4399 29 22.6099 29 27.3899 27.41C28.7399 26.96 29.3199 25.38 28.5899 24.16L27.3199 22.04C26.9699 21.46 26.6899 20.43 26.6899 19.76V17.66C26.6799 14 23.6799 11 20.0199 11Z"
         stroke="white"
-        stroke-width="1.5"
-        stroke-miterlimit="10"
-        stroke-linecap="round"
+        strokeWidth="1.5"
+        strokeMiterlimit="10"
+        strokeLinecap="round"
       />
       <path
         d="M23.3299 27.82C23.3299 29.65 21.8299 31.15 19.9999 31.15C19.0899 31.15 18.2499 30.77 17.6499 30.17C17.0499 29.57 16.6699 28.73 16.6699 27.82"
         stroke="white"
-        stroke-width="1.5"
-        stroke-miterlimit="10"
+        strokeWidth="1.5"
+        strokeMiterlimit="10"
       />
       <circle cx="26" cy="12" r="3" fill="#D92C2C" />
     </svg>
