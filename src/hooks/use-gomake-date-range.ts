9b828fe6ef7e182f55082c04ad676b212a597d5d
riadFadila <riad@gomake.net>
2023-04-26 10:53:25 +0300

    bug fix

--- src/hooks/use-gomake-date-range.ts
+++ src/hooks/use-gomake-date-range.ts
@@ -26,11 +26,11 @@ const useGomakeDateRange = () => {
     }, []);
 
     const setNullDate = useCallback(() => {
-        setDate({startDate: null, endDate: null})
+        setDate({startDate: undefined, endDate: undefined})
     }, [])
 
     const isNullDate = useCallback(() => {
-        return date.endDate === null && date.startDate === null
+        return date.endDate === undefined && date.startDate === undefined
     }, [date]);
 
     const isToday = useCallback(() => {
@@ -50,7 +50,7 @@ const useGomakeDateRange = () => {
             return t('dashboard-widget.tomorrow') + ' ' + t('dashboard-widget.tasks');
         }
 
-        if (date.startDate === null && date.endDate === null) {
+        if (date.startDate === undefined && date.endDate === undefined) {
             return t('dashboard-widget.tasks') + ' ' + t('dashboard-widget.lateMissions')
         }
 
