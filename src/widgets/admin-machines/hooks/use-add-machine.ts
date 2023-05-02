1f93055bf1bb5630cbd74a081c2b059cc8991627
riadFadila <riad@gomake.net>
2023-05-02 20:55:20 +0300

    show message after calling api

--- src/widgets/admin-machines/hooks/use-add-machine.ts
+++ src/widgets/admin-machines/hooks/use-add-machine.ts
@@ -1,9 +1,10 @@
 import {useCallback, useEffect} from "react";
 import {useRecoilValue, useSetRecoilState} from "recoil";
-import {useGomakeAxios} from "@/hooks";
+import {useGomakeAxios, useSnackBar} from "@/hooks";
 import {useRouter} from "next/router";
 import {machineState} from "@/widgets/admin-machines/state/machine-state";
 import {initState} from "@/widgets/admin-machines/state/init-state";
+import {useTranslation} from "react-i18next";
 
 const useAddMachine = () => {
     const {callApi} = useGomakeAxios()
@@ -11,7 +12,8 @@ const useAddMachine = () => {
     const setState = useSetRecoilState(machineState);
     const router = useRouter();
     const {categoryId} = router.query;
-
+    const { setSnackbarStateValue } = useSnackBar();
+    const {t} = useTranslation();
     useEffect(() => {
         const stateInit = initState[categoryId as string] ? initState[categoryId as string] : {};
         setState(stateInit);
@@ -23,8 +25,18 @@ const useAddMachine = () => {
     const onClickAddMachine = useCallback( () => {
         callApi('POST', '/v1/administrator/add-machine', {...state}).then(res => {
             if (res?.success) {
+                setSnackbarStateValue({
+                    state: true,
+                    message: t("modal.addedSusuccessfully"),
+                    type: "sucess",
+                });
                  router.push(`/admin/machine/category/${state?.category}`).then()
-
+            } else {
+                setSnackbarStateValue({
+                    state: true,
+                    message: t("modal.addedfailed"),
+                    type: "error",
+                });
             }
         })
     }, [state]);
--- src/widgets/admin-machines/hooks/use-update-machine.ts
+++ src/widgets/admin-machines/hooks/use-update-machine.ts
@@ -1,19 +1,32 @@
-import {useGomakeAxios} from "@/hooks";
+import {useGomakeAxios, useSnackBar} from "@/hooks";
 import {useRecoilValue} from "recoil";
 import {machineState as STATE} from "@/widgets/admin-machines/state/machine-state";
 import {useCallback} from "react";
 import {useRouter} from "next/router";
 import {ECategoryId} from "@/widgets/admin-machines/enums/category-id";
+import {useTranslation} from "react-i18next";
 
 const useUpdateMachine = () => {
     const {callApi} = useGomakeAxios();
     const machineState = useRecoilValue(STATE);
     const router = useRouter();
-
+    const { setSnackbarStateValue } = useSnackBar();
+    const {t} = useTranslation();
     const updateMachine = async () => {
        const result = await callApi('POST', '/v1/administrator/update-machine', {...machineState});
         if (result?.success) {
+            setSnackbarStateValue({
+                state: true,
+                message: t("modal.addedSusuccessfully"),
+                type: "sucess",
+            });
             await router.push(`/admin/machine/category/${machineState?.category}`)
+        } else {
+            setSnackbarStateValue({
+                state: true,
+                message: t("modal.addedfailed"),
+                type: "error",
+            });
         }
     }
 
