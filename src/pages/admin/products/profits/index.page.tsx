e67dfa77fa029e9c773b0ccb413d2b5ddd367c49
Ahmed <shehab@gomake.net>
2023-05-10 10:00:50 +0300

    add profit page

---
+++ src/pages/admin/products/profits/index.page.tsx
@@ -0,0 +1,12 @@
+import { useTranslation } from "react-i18next";
+
+import { AdminAuthLayout } from "@/layouts";
+export default function Profits() {
+  const { t } = useTranslation();
+
+  return (
+    <AdminAuthLayout>
+      <div>ggg</div>
+    </AdminAuthLayout>
+  );
+}
