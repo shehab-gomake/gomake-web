abddef3eb14a8d1ec93c3315c56bfff4114d1229
faraj@gomake.net <faraj@gomake.net>
2023-06-21 11:07:37 +0300

    fix

--- src/pages/materials-new/tubes/widgets/header-table/index.tsx
+++ src/pages/materials-new/tubes/widgets/header-table/index.tsx
@@ -8,11 +8,7 @@ const HeaderTableWidget = ({ setSheetCheckStore, sheetCheckStore, index }) => {
   const { clasess } = useStyle();
 
   return (
-    <div
-      style={{
-        ...clasess.header,
-      }}
-    >
+    <div style={clasess.header}>
       <div style={clasess.checkboxHeaderContainer}>
         <Checkbox
           onChange={() => {
@@ -21,33 +17,50 @@ const HeaderTableWidget = ({ setSheetCheckStore, sheetCheckStore, index }) => {
           checked={sheetCheckStore}
         />
       </div>
-      <div style={clasess.rowWeightContainer}>
-        <div style={index % 2 == 0 ? clasess.bodyRow : clasess.secondRow}>
-          <div style={clasess.sizeContainer}>
-            {t("materials.inputs.weight")}
-          </div>
-          <div style={clasess.thiknessContainer}>
-            {t("materials.inputs.price")}
-          </div>
-          <div style={clasess.costsContainer}>
-            {t("materials.inputs.diameter")}
-          </div>
-          <div style={clasess.directionContainer}>
-            {t("materials.inputs.lenght")}
-          </div>
-
-          <div style={clasess.activeContainer}>
-            {t("products.actions.active")}
-          </div>
-          <div style={clasess.currencyContainer}>
-            {t("materials.inputs.currency")}
-          </div>
-          <div style={clasess.stokContainer}>
-            {t("materials.sheetPaper.stock")}
-          </div>
-        </div>
+      <div style={clasess.sizeContainer}>{t("materials.inputs.weight")}</div>
+      <div style={clasess.thiknessContainer}>
+        {" "}
+        {t("materials.inputs.price")}
+      </div>
+      <div style={clasess.costsContainer}>{t("materials.inputs.diameter")}</div>
+      <div style={clasess.directionContainer}>
+        {" "}
+        {t("materials.inputs.lenght")}
+      </div>
+      <div style={clasess.activeContainer}>{t("products.actions.active")}</div>
+      <div style={clasess.currencyContainer}>
+        {" "}
+        {t("materials.inputs.currency")}
+      </div>
+      <div style={clasess.stokContainer}>
+        {" "}
+        {t("materials.sheetPaper.stock")}
       </div>
     </div>
+
+    //   <div style={index % 2 == 0 ? clasess.bodyRow : clasess.secondRow}>
+    //     <div style={clasess.sizeContainer}></div>
+    //     <div style={clasess.thiknessContainer}>
+    //
+    //     </div>
+    //     <div style={clasess.costsContainer}>
+    //
+    //     </div>
+    //     <div style={clasess.directionContainer}>
+    //
+    //     </div>
+
+    //     <div style={clasess.activeContainer}>
+    //
+    //     </div>
+    //     <div style={clasess.currencyContainer}>
+    //
+    //     </div>
+    //     <div style={clasess.stokContainer}>
+    //
+    //     </div>
+    //   </div>
+    // </div>
   );
 };
 export { HeaderTableWidget };
