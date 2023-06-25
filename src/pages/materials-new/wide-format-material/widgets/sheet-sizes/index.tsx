c3772c5b037ac13d4f5c570c5b9b1457211c1f56
Ahmed <shehab@gomake.net>
2023-06-25 14:39:11 +0300

    add transate

--- src/pages/materials-new/wide-format-material/widgets/sheet-sizes/index.tsx
+++ src/pages/materials-new/wide-format-material/widgets/sheet-sizes/index.tsx
@@ -24,7 +24,7 @@ const SheetSizesWidget = ({
       "POST",
       `/v1/wide-format-material/size-id-settngs`,
       {
-        categoryName: selectedMaterials,
+        categoryName: selectedMaterials?.key,
         supplierId: selectedSupplier,
         actionType: 3,
         data: [
@@ -56,7 +56,7 @@ const SheetSizesWidget = ({
       "POST",
       `/v1/wide-format-material/size-id-settngs`,
       {
-        categoryName: selectedMaterials,
+        categoryName: selectedMaterials?.key,
         supplierId: selectedSupplier,
         actionType: 4,
         data: [
