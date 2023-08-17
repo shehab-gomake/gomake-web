ba922cf8c28d945521fa8e6e4ac44da017d36c9d
Ahmed <shehab@gomake.net>
2023-08-17 10:09:49 +0300

    add edit index and delete row in size values

---
+++ src/icons/re-order-icon.tsx
@@ -0,0 +1,35 @@
+const ReOrderIcon = (props: any) => {
+  return (
+    <svg
+      width="12"
+      height="10"
+      viewBox="0 0 12 10"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M1 1H11"
+        stroke="#ABACD3"
+        stroke-width="1.5"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M1 5H11"
+        stroke="#ABACD3"
+        stroke-width="1.5"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M1 9H11"
+        stroke="#ABACD3"
+        stroke-width="1.5"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+  );
+};
+
+export { ReOrderIcon };
