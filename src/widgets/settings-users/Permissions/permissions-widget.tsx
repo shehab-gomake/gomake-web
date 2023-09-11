3be5a34cc3784405dd2700791dee62b9039825b4
riadFadila <riad@gomake.net>
2023-09-11 11:18:19 +0300

    wip

--- src/widgets/settings-users/Permissions/permissions-widget.tsx
+++ src/widgets/settings-users/Permissions/permissions-widget.tsx
@@ -1,66 +1,78 @@
-import { GoMakeTextInputIcon } from "@/components";
+import {GoMakeTextInputIcon} from "@/components";
 import StickyFirstColumnTable from "@/components/StickyTable/StickyFirstColumnTable";
-import { AddPlusIcon, SearchIcon } from "@/icons";
-import { FONT_FAMILY } from "@/utils/font-family";
-import { HeaderTitle } from "@/widgets/header-title";
-import { useState } from "react";
-import { InputAdornment, Tab, Table,TableBody, TableCell, TableContainer, TableHead, TableRow, Tabs, ThemeProvider, createMuiTheme } from "@mui/material";
-import { useTranslation } from "react-i18next";
-import { useStyle } from "./style";
-import { AddRoleModal } from "./modals";
-import { useSettings } from "./use-settings";
-import {  SecondaryTabsComponent } from "@/components/tabs/secondary-tabs";
+import {AddPlusIcon, SearchIcon} from "@/icons";
+import {FONT_FAMILY} from "@/utils/font-family";
+import {HeaderTitle} from "@/widgets/header-title";
+import {useState} from "react";
+import {
+    InputAdornment,
+    Tab,
+    Table,
+    TableBody,
+    TableCell,
+    TableContainer,
+    TableHead,
+    TableRow,
+    Tabs,
+    ThemeProvider,
+    createMuiTheme
+} from "@mui/material";
+import {useTranslation} from "react-i18next";
+import {useStyle} from "./style";
+import {AddRoleModal} from "./modals";
+import {useSettings} from "./use-settings";
+import {SecondaryTabsComponent} from "@/components/tabs/secondary-tabs";
 
 
 const PermissionsWidget = () => {
-  const {t} = useTranslation();
-  const {classes} = useStyle();
-  const theme = createMuiTheme({
-      palette: {
-        secondary: {
-          main: '#ED028C',
+    const {t} = useTranslation();
+    const {classes} = useStyle();
+    const theme = createMuiTheme({
+        palette: {
+            secondary: {
+                main: '#ED028C',
+            },
         },
-      },
     });
     const handleTabChange = (event, newValue) => {
-      setSelectedTab(newValue);
-    
+        setSelectedTab(newValue);
+
     };
     const [isNewRole, setisNewRole] = useState(false);
     const onClickCloseNewRole = () => {
-      setisNewRole(false);
+        setisNewRole(false);
     };
     const [selectedTab, setSelectedTab] = useState(0);
-    const {tableHeaders,permissionsRoles,groups,table,permissions} =
-    useSettings();
+    const {tableHeaders, permissionsRoles, groups, table, permissions, onSelectTab} =
+        useSettings();
     const tabs = [];
     groups?.forEach((row) => {
-        tabs.push({ title: row.name});
-      });
-   
-  return (
-      <div style={classes.mainContainer}>
-          <div style={{display:"flex",justifyContent:"flex-end",alignItems:"flex-end",width:"100%"}}>
-                  <div
-                      style={classes.addProductBtnStyle}
-                      onClick={() => setisNewRole(true)}
-                  
-                  >
-                      <AddPlusIcon stroke="#101020"/>
-                      <div style={classes.addProductBtnText}>
-                          {t("permissionsSettings.AddRole")}
-                      </div>
-                  </div>
+        tabs.push({title: row.name});
+    });
 
-          </div>
-          
-          <div style={classes.mainHeadecontainer}>
-                   
-              <HeaderTitle title={t("permissionsSettings.title")} marginBottom={3}/>
-          </div>
-          <div style={{width:"98%"}}>
-          <SecondaryTabsComponent tabs={tabs} />
-              {/* <ThemeProvider theme={theme}>
+    return (
+        <div style={classes.mainContainer}>
+            <div style={{display: "flex", justifyContent: "flex-end", alignItems: "flex-end", width: "100%"}}>
+                <div
+                    style={classes.addProductBtnStyle}
+                    onClick={() => setisNewRole(true)}
+
+                >
+                    <AddPlusIcon stroke="#101020"/>
+                    <div style={classes.addProductBtnText}>
+                        {t("permissionsSettings.AddRole")}
+                    </div>
+                </div>
+
+            </div>
+
+            <div style={classes.mainHeadecontainer}>
+
+                <HeaderTitle title={t("permissionsSettings.title")} marginBottom={3}/>
+            </div>
+            <div style={{width: "98%"}}>
+                <SecondaryTabsComponent tabs={tabs} onSelectTab={onSelectTab}/>
+                {/* <ThemeProvider theme={theme}>
                   <Tabs sx={{ minHeight: 'unset', minWidth: 'unset' }} value={selectedTab} onChange={handleTabChange} textColor="secondary"  indicatorColor="secondary">
                   {groups?.map(x => (
                         <Tab
@@ -81,36 +93,36 @@ const PermissionsWidget = () => {
                     ))}
                   </Tabs>   
               </ThemeProvider> */}
-        
-          </div>
-          <div style={{display:"flex",width:"100%",justifyContent:"flex-end",flexDirection:"row"}}>
-              <div style={classes.subHeaderRightSide}>
-                          <GoMakeTextInputIcon
-                              style={classes.searchInputContainer}
-                              placeholder={t("header.search")}
-                              startAdornment={
-                                  <InputAdornment position="start">
-                                      <div style={classes.iconStyle}>
-                                          <SearchIcon/>
-                                      </div>
-                                  </InputAdornment>
-                              }
-                          />
-              </div>
-          </div>
 
-        <StickyFirstColumnTable columns={tableHeaders} data={table} /> 
+            </div>
+            <div style={{display: "flex", width: "100%", justifyContent: "flex-end", flexDirection: "row"}}>
+                <div style={classes.subHeaderRightSide}>
+                    <GoMakeTextInputIcon
+                        style={classes.searchInputContainer}
+                        placeholder={t("header.search")}
+                        startAdornment={
+                            <InputAdornment position="start">
+                                <div style={classes.iconStyle}>
+                                    <SearchIcon/>
+                                </div>
+                            </InputAdornment>
+                        }
+                    />
+                </div>
+            </div>
 
-          <AddRoleModal
-              openModal={isNewRole}
-              modalTitle={t("permissionsSettings.AddNewRole")}
-              onClose={onClickCloseNewRole}
-           
-          />
-  
-         
-      </div>
-  )
+            <StickyFirstColumnTable columns={tableHeaders} data={table}/>
+
+            <AddRoleModal
+                openModal={isNewRole}
+                modalTitle={t("permissionsSettings.AddNewRole")}
+                onClose={onClickCloseNewRole}
+
+            />
+
+
+        </div>
+    )
 }
 
 export {PermissionsWidget};
--- src/widgets/settings-users/Permissions/use-settings.tsx
+++ src/widgets/settings-users/Permissions/use-settings.tsx
@@ -1,93 +1,112 @@
-import { useCallback, useEffect, useState } from "react";
-import { useTranslation } from "react-i18next";
-import { useGomakeAxios } from "@/hooks";
+import {useCallback, useEffect, useState} from "react";
+import {useTranslation} from "react-i18next";
+import {useGomakeAxios} from "@/hooks";
 
 
-import { useStyle } from "./style";
+import {useStyle} from "./style";
 import Switch from "@mui/material/Switch";
-import {ThemeProvider,  createMuiTheme, createTheme } from "@mui/material/styles";
-import { getAndSetPermissions, getAndSetPermissionsRolesRelation, getAndSetRoles } from "@/services/hooks/get-set-permissionRoles";
+import {ThemeProvider, createMuiTheme, createTheme} from "@mui/material/styles";
+import {
+    getAndSetPermissions,
+    getAndSetPermissionsRolesRelation,
+    getAndSetRoles
+} from "@/services/hooks/get-set-permissionRoles";
 import {getPermissionRolesRelationsByGroupId} from "@/services/hooks/get-permissions-by-id-group";
 
 const useSettings = () => {
-  const { callApi } = useGomakeAxios();
-  const { classes } = useStyle();
+    const {callApi} = useGomakeAxios();
+    const {classes} = useStyle();
 
-  const [groups,setgroups] = useState([]);
-  const [permissionsRoles,setpermissionsRoles] = useState([]);
-  const [tableHeaders, setTableHeaders] = useState([]);
-  const [permissions, setpermissions] = useState([]);
-  const [table, setTable] = useState([]);
-  const { t } = useTranslation();
-  const theme = createTheme({
-    palette: {
-      secondary: {
-        main: '#ED028C',
+    const [groups, setgroups] = useState([]);
+    const [permissionsRoles, setpermissionsRoles] = useState([]);
+    const [tableHeaders, setTableHeaders] = useState([]);
+    const [permissions, setpermissions] = useState([]);
+    const [table, setTable] = useState([]);
+    const [selectedTab, setSelectedTab] = useState<{id: string; title: string}>();
+    const {t} = useTranslation();
+
+
+    useEffect(()=> {
+      if (!!selectedTab) {
+
+      }
+    }, [selectedTab])
+    const onSelectTab = (index: number) => {
+        const newSelectedTab = groups[index];
+        if (!!newSelectedTab) {
+          setSelectedTab(selectedTab)
       }
     }
-  });
-  const getSwitch = (roleId,permissionId)=>{
-    return (
-      <ThemeProvider theme={theme}>
-          <Switch  color="secondary" checked={true} />
-    </ThemeProvider> 
-    );
-  
-  }
-  const getSwitchFale = (roleId,permissionId)=>{
-    return (
-      <ThemeProvider  theme={theme}>   
-         <Switch  color="secondary"checked={false}/>
-      </ThemeProvider>
-    );
-  }
-  const getPermissionRolesRelations = useCallback(async () => {
-    const data = await getAndSetPermissionsRolesRelation(callApi, setpermissionsRoles);
-    const roles = [...data.roles]; // Create a new array and copy elements from 'data.roles'
-    roles.unshift([]); // Adds an empty array to the beginning of the new 'roles' array
-    setTableHeaders(roles);
-    setgroups(data.groups)
-    setpermissions(data.permissions);
-    var col =[]
-    const row = [col];
-    const table2 = [];
-    data.permissions.forEach(permission => {
-      var row = [];
-      row.push(permission.description);
-      roles.forEach(role => {
-        var relation =  permission.rolesPermissionsRelationships.find(x=> x.roleId == role.id)
-        var col = relation ? getSwitch(role.id,permission.id) : getSwitchFale(role.id,permission.id)
-        row.push(col);
-      });
-      table2.push(row)
-    })
-    setTable(table2);
-  
-  },[callApi, setpermissionsRoles, t])
+    const theme = createTheme({
+        palette: {
+            secondary: {
+                main: '#ED028C',
+            }
+        }
+    });
+    const getSwitch = (roleId, permissionId) => {
+        return (
+            <ThemeProvider theme={theme}>
+                <Switch color="secondary" checked={true}/>
+            </ThemeProvider>
+        );
+
+    }
+    const getSwitchFale = (roleId, permissionId) => {
+        return (
+            <ThemeProvider theme={theme}>
+                <Switch color="secondary" checked={false}/>
+            </ThemeProvider>
+        );
+    }
+    const getPermissionRolesRelations = useCallback(async () => {
+        const data = await getAndSetPermissionsRolesRelation(callApi, setpermissionsRoles);
+        const roles = [...data.roles]; // Create a new array and copy elements from 'data.roles'
+        roles.unshift([]); // Adds an empty array to the beginning of the new 'roles' array
+        setTableHeaders(roles);
+        setgroups(data.groups)
+        setpermissions(data.permissions);
+        var col = []
+        const row = [col];
+        const table2 = [];
+        data.permissions.forEach(permission => {
+            var row = [];
+            row.push(permission.description);
+            roles.forEach(role => {
+                var relation = permission.rolesPermissionsRelationships.find(x => x.roleId == role.id)
+                var col = relation ? getSwitch(role.id, permission.id) : getSwitchFale(role.id, permission.id)
+                row.push(col);
+            });
+            table2.push(row)
+        })
+        setTable(table2);
+
+    }, [callApi, setpermissionsRoles, t])
 
 
+    const getAndSetPermissionRolesRelationsByGroupId = useCallback(async () => {
+        const data = await getPermissionRolesRelationsByGroupId(callApi, setpermissionsRoles);
+    }, [callApi, setpermissionsRoles, t])
 
-  const getAndSetPermissionRolesRelationsByGroupId = useCallback(async () => {
-    const data = await getPermissionRolesRelationsByGroupId(callApi,setpermissionsRoles);
-  },[callApi,setpermissionsRoles,t])
-  useEffect(() => {
-    const fetchData = async () => {
-      await getPermissionRolesRelations();
+    useEffect(() => {
+        const fetchData = async () => {
+            await getPermissionRolesRelations();
+        };
+
+        fetchData();
+    }, [getPermissionRolesRelations]);
+
+
+    console.log(table)
+
+    return {
+        tableHeaders,
+        permissionsRoles,
+        groups,
+        permissions,
+        table,
+        onSelectTab
     };
-  
-    fetchData();
-  }, [getPermissionRolesRelations]);
-
-
-  console.log(table)
-
-  return {
-    tableHeaders,
-    permissionsRoles,
-    groups,
-    permissions,
-    table
-  };
 };
 
-export { useSettings };
+export {useSettings};
