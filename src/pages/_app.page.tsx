b3afff37ea28a3abe07a4fe6b335ab502721be63
Ahmed <shehab@gomake.net>
2023-09-12 12:31:40 +0300

    return en lang

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -33,8 +33,8 @@ i18n
         },
       },
     },
-    lng: "he", // if you're using a language detector, do not define the lng option
-    fallbackLng: "he",
+    lng: "en", // if you're using a language detector, do not define the lng option
+    fallbackLng: "en",
 
     interpolation: {
       escapeValue: false, // react already safes from xss => https://www.i18next.com/translation-function/interpolation#unescape
