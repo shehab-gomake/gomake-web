7a1de60e733e3a3fa3fcb16197d240b85255aee8
riadFadila <riad@gomake.net>
2023-09-12 11:31:30 +0300

    add value property to search input component

--- src/components/form-inputs/search-input-component.tsx
+++ src/components/form-inputs/search-input-component.tsx
@@ -5,9 +5,10 @@ import {useStyle} from "@/components/form-inputs/style";
 import {useTranslation} from "react-i18next";
 interface ISearchInputProps {
     onChange: (value: string) => void;
+    value?: string;
 }
 
-const SearchInputComponent = ({onChange}: ISearchInputProps) => {
+const SearchInputComponent = ({onChange, value}: ISearchInputProps) => {
     const {classes} = useStyle();
     const {t} = useTranslation();
   return (
@@ -15,6 +16,7 @@ const SearchInputComponent = ({onChange}: ISearchInputProps) => {
           onChange={(e) => {onChange(e.target.value)}}
           style={classes.searchInput}
           placeholder={t("header.search")}
+          value={value}
           startAdornment={
               <InputAdornment position="start">
                   <div style={classes.iconStyle}>
