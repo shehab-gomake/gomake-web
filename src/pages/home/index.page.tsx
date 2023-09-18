c146af4a8589849abd3db4641106409828c63f0a
Ahmed <shehab@gomake.net>
2023-09-18 13:41:58 +0300

    clean code for home and login

--- src/pages/home/index.page.tsx
+++ src/pages/home/index.page.tsx
@@ -1,10 +1,7 @@
 import { CustomerAuthLayout } from "@/layouts";
 import { HomePageComponentForAdmin } from "@/pages-components/admin/home/home";
-import { useTranslation } from "react-i18next";
 
 export default function Home() {
-  const { t } = useTranslation();
-
   return (
     <CustomerAuthLayout>
       <HomePageComponentForAdmin isAdmin={false} />
