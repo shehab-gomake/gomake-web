d0e39d7bd6accf6acb61fbd86fa001a8945ceca1
MohnadGomake <95920049+MohnadGomake@users.noreply.github.com>
2023-11-15 17:36:43 +0200

    Update use-date-format.ts

--- src/hooks/use-date-format.ts
+++ src/hooks/use-date-format.ts
@@ -1,11 +1,18 @@
 import moment from "moment-timezone";
+import {useTranslation} from "react-i18next";
 
 
 const useDateFormat = () => {
+    const {t} = useTranslation();
+    const dir: "rtl" | "ltr" = t("direction");
     const GetDateFormat = (date: Date) => {
         debugger;
-        const utcDate = moment.utc(date).tz("UTC");
-        return utcDate.local().format();
+        const utcDate = moment.utc(date,"DD-MM-YYYY h:mm");
+        let format = "YYYY-MM-DD HH:mm";
+        if(dir == "rtl"){
+            format = "HH:mm YYYY-MM-DD"
+        }
+        return utcDate.local().format(format);
     }
     return {
         GetDateFormat: GetDateFormat
