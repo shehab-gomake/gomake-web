fe070e2a44db9441575a2c1052b6a4565f9d039a
MohnadGomake <95920049+MohnadGomake@users.noreply.github.com>
2023-11-15 16:51:04 +0200

    updates

--- src/hooks/use-date-format.ts
+++ src/hooks/use-date-format.ts
@@ -3,6 +3,7 @@ import moment from "moment-timezone";
 
 const useDateFormat = () => {
     const GetDateFormat = (date: Date) => {
+        debugger;
         const utcDate = moment.utc(date).tz("UTC");
         return utcDate.local().format();
     }
