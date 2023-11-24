317f725af21fee653f92c3df3a3d7f32974ad004
riadFadila <riad@gomake.net>
2023-11-24 12:13:20 +0200

    update work flow after updating action

--- src/widgets/product-pricing-widget/components/action/use-action-update-values.ts
+++ src/widgets/product-pricing-widget/components/action/use-action-update-values.ts
@@ -1,4 +1,4 @@
-import {IOutput} from "@/widgets/product-pricing-widget/interface";
+import {IOutput, IWorkFlowAction} from "@/widgets/product-pricing-widget/interface";
 import {useRecoilState} from "recoil";
 import {workFlowsState} from "@/widgets/product-pricing-widget/state";
 
@@ -16,9 +16,9 @@ const useActionUpdateValues = () => {
 
     const updateCost = (cost: string, profit: string, actionId: string) => {
         const price = +cost + (+profit * +cost / 100);
-        setWorkFlows(workFlows.map(flow => !flow.selected ? flow : {
-            ...flow,
-            actions: flow.actions.map(action => action.actionId !== actionId ? action : {
+        const selectedWorkFlow = workFlows?.find(flow => flow.selected);
+        if (selectedWorkFlow) {
+            const updatedActions = selectedWorkFlow.actions?.map(action => action.actionId !== actionId ? action : {
                 ...action,
                 totalCostO: {
                     ...action.totalCostO,
@@ -29,32 +29,36 @@ const useActionUpdateValues = () => {
                     values: [price.toString()]
                 }
             })
-        }));
-    }
+
+            updateSelectedWorkFlow(updatedActions);
+        }
+    };
+
     const updateProfit = (cost: string, profit: string, actionId: string) => {
         const price = +cost + (+profit * +cost / 100);
-        setWorkFlows(workFlows.map(flow => !flow.selected ? flow : {
-            ...flow,
-            actions: flow.actions.map(action => action.actionId !== actionId ? action : {
+        const selectedWorkFlow = workFlows?.find(flow => flow.selected);
+        if (selectedWorkFlow) {
+            const updatedActions = selectedWorkFlow.actions?.map(action => action.actionId !== actionId ? action : {
                 ...action,
                 profitO: {
-                  ...action.profitO,
-                  values: [profit]
+                    ...action.profitO,
+                    values: [profit]
                 },
                 totalPriceO: {
                     ...action.totalPriceO,
                     values: [price.toString()]
                 }
             })
-        }));
+            updateSelectedWorkFlow(updatedActions);
+        }
     }
 
     const updatePrice = (price: string, cost: string, actionId: string) => {
         const profit = +price - +cost;
         const profitPercentage = profit / +cost * 100;
-        setWorkFlows(workFlows.map(flow => !flow.selected ? flow : {
-            ...flow,
-            actions: flow.actions.map(action => action.actionId !== actionId ? action : {
+        const selectedWorkFlow = workFlows?.find(flow => flow.selected);
+        if (selectedWorkFlow) {
+            const updatedActions = selectedWorkFlow.actions?.map(action => action.actionId !== actionId ? action : {
                 ...action,
                 profitO: {
                     ...action.profitO,
@@ -65,7 +69,33 @@ const useActionUpdateValues = () => {
                     values: [price]
                 }
             })
-        }));
+
+            updateSelectedWorkFlow(updatedActions)
+        }
+    }
+
+    const updateSelectedWorkFlow = (actions: IWorkFlowAction[]) => {
+        const totalPrice = actions.reduce((sum, action) => sum + +action.totalPriceO.values[0], 0);
+        const totalCost = actions.reduce((sum, action) => sum + +action.totalCostO.values[0], 0);
+        const profit = (totalPrice - totalCost) / totalCost * 100;
+        setWorkFlows(workFlows.map(flow => !flow.selected ? flow : {
+            ...flow,
+            actions: actions,
+            totalPrice,
+            totalPriceO: {
+                ...flow.totalPriceO,
+                values: [totalPrice.toString()]
+            },
+            totalCost: totalCost,
+            totalCostO: {
+                ...flow.totalCostO,
+                values: [totalCost.toString()]
+            },
+            profitO: {
+                ...flow.profitO,
+                values: [profit.toString()]
+            }
+        }))
     }
 
     return {
