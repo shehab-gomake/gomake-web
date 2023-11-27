839e1882ca20c10475eeeeef2c3ac29caafd9fb8
riadFadila <riad@gomake.net>
2023-11-27 13:40:18 +0200

    fix materials height in small screens

--- src/widgets/materials-widget/materials-widget.tsx
+++ src/widgets/materials-widget/materials-widget.tsx
@@ -44,27 +44,30 @@ const MaterialsWidget = () => {
         replace
     } = useMaterials();
 
-    const Side = () => 
-    <Stack  gap={'10px'} direction={'column'} >
-    <SecondaryButton variant={'text'} href={'/materials'}  startIcon={ dir === 'ltr' ?  <ArrowBackIcon/> : <ArrowForwardIcon/> } style={{gap:5}} >{t("materials.buttons.back")}
-      </SecondaryButton>
-    <SideList list={materialsCategoriesList()} selectedItem={materialCategory?.toString()}
-                                 onSelect={onSelectCategory}
-                                 title={'choose category'}>
-        <Stack gap={'10px'} direction={'row'} justifyContent={'space-between'}>
-            <PrimaryButton onClick={downloadExcelFile} variant={'contained'}>Download</PrimaryButton>
-            <input ref={elementRef} onChange={uploadExcelFile} type="file" accept=".xlsx"  hidden={true} />
-            <SecondaryButton onClick={() => elementRef && elementRef.current.click()} variant={'contained'}>Upload</SecondaryButton>
+    const Side = () =>
+        <Stack gap={'10px'} direction={'column'}>
+            <SecondaryButton variant={'text'} href={'/materials'}
+                             startIcon={dir === 'ltr' ? <ArrowBackIcon/> : <ArrowForwardIcon/>}
+                             style={{gap: 5}}>{t("materials.buttons.back")}
+            </SecondaryButton>
+            <SideList list={materialsCategoriesList()} selectedItem={materialCategory?.toString()}
+                      onSelect={onSelectCategory}
+                      title={'choose category'}>
+                <Stack gap={'10px'} direction={'row'} justifyContent={'space-between'}>
+                    <PrimaryButton onClick={downloadExcelFile} variant={'contained'}>Download</PrimaryButton>
+                    <input ref={elementRef} onChange={uploadExcelFile} type="file" accept=".xlsx" hidden={true}/>
+                    <SecondaryButton onClick={() => elementRef && elementRef.current.click()}
+                                     variant={'contained'}>Upload</SecondaryButton>
+                </Stack>
+            </SideList>
         </Stack>
-    </SideList>
-    </Stack>
 
     useEffect(() => {
         if (materialType && materialCategories.length > 0) {
             if (!materialCategory || !materialCategories.some(category => category.categoryKey === materialCategory)) {
                 replace({
                     pathname: materialType.toString(),
-                    query: { materialCategory: materialCategories[0].categoryKey }
+                    query: {materialCategory: materialCategories[0].categoryKey}
                 })
             }
         }
@@ -87,12 +90,12 @@ const MaterialsWidget = () => {
                 getPrintHouseMaterialCategorySuppliers(materialType?.toString(), materialCategory?.toString()).then();
             }
         }
-    }, [materialType, materialCategory ,supplierId])
+    }, [materialType, materialCategory, supplierId])
 
 
     return (
         <div style={{maxHeight: convertHeightToVH(SCREEN_HEIGHT - HEADER_HEIGHT - 20), overflow: 'hidden'}}>
-            <SideBarContainer side={Side()} header={materialType?.toString()} subHeader={''} >
+            <SideBarContainer side={Side()} header={materialType?.toString()} subHeader={''}>
                 {materialCategory && <Stack gap={2}>
                     <Stack direction={'row'} justifyContent={'space-between'} alignItems={'flex-start'}>
                         <h4 style={classes.subHeader}>{materialCategory?.toString()}</h4>
@@ -100,7 +103,7 @@ const MaterialsWidget = () => {
                     </Stack>
                     {
                         materialCategoryData.length > 0 ?
-                            <PrimaryTable  rows={tableRows} headers={tableHeaders()}/> :
+                            <div style={{paddingBottom: '30px'}}><PrimaryTable rows={tableRows} headers={tableHeaders()}/></div> :
                             <div style={classes.noData}>
                                 {t("materials.sheetPaper.supplierAddedSheetYet")}
                                 <span
@@ -114,8 +117,8 @@ const MaterialsWidget = () => {
                             </div>
                     }
                 </Stack>}
-                <AddSupplierModal/>
             </SideBarContainer>
+            <AddSupplierModal/>
         </div>
     );
 }
