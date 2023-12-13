2b273d38e811c541c4188a6e6965bbc92272eef7
lamaAnbtawe <106581571+lamaAnbtawe@users.noreply.github.com>
2023-12-13 13:40:56 +0200

    valdita the new value of document numbering

--- src/widgets/settings-documenting/documentNumbering/document-numbering-settings.tsx
+++ src/widgets/settings-documenting/documentNumbering/document-numbering-settings.tsx
@@ -3,18 +3,25 @@ import { NumberingTable } from "./components/numbering-table/numbering-table";
 import { useTranslation } from "react-i18next";
 import { GoMakeModal } from "@/components";
 import { useStyle } from "./style";
-import { Stack } from "@mui/material";
+import { Stack, Tooltip } from "@mui/material";
 import { FormInput } from "@/components/form-inputs/form-input";
 import { IInput } from "@/components/form-inputs/interfaces";
 import { useDocumentNumbers } from "./use-documents-numbers";
 import { editDocumentInputs, editDocumentInputs1 } from "./inputs/edit-modal-inputs";
 import { SecondaryButton } from "@/components/button/secondary-button";
+import { useState } from "react";
 const DocumentNumbering = () => {
     const { classes } = useStyle();
     const { openModal, setOpenModal, document, setDocument, onUpdateDocument } = useDocumentNumbers();
     const { t } = useTranslation();
+    const [showTooltip, setShowTooltip] = useState(false);
 
     const onChangeInputs = (key, value) => {
+        if (key === "value" && value <= document.nextValue) {
+            setShowTooltip(true);
+        } else {
+            setShowTooltip(false);
+        }
         setDocument({ ...document, [key]: value })
     }
 
@@ -26,13 +33,19 @@ const DocumentNumbering = () => {
                     insideStyle={classes.insideStyle}
                     headerPadding={15}
                     openModal={openModal}
-                    onClose={() => setOpenModal(false)}
-                    modalTitle={ t("documentingSettings.editDefinition") +  " - " + document?.documentName }
-                    >
-                    <Stack direction={'column'} gap={"20px"}  padding={3}>
+                    onClose={() => { setOpenModal(false); setShowTooltip(false); }}
+                    modalTitle={t("documentingSettings.editDefinition") + " - " + document?.documentName}
+                >
+                    <Stack direction={'column'} gap={"20px"} padding={3}>
                         <Stack direction={'row'} gap={"19px"} >
                             {
-                                editDocumentInputs(document).map(item => <FormInput input={item as IInput} changeState={onChangeInputs} error={false} readonly={!!item.readonly} />)
+                                editDocumentInputs(document).map(item => (
+                                    <div ><FormInput input={item as IInput} changeState={onChangeInputs} error={false} readonly={!!item.readonly} />
+                                        {showTooltip && item.name === "value" && (
+                                            <Tooltip title={t("documentingSettings.nextValueTip") + " " + document.nextValue} arrow>
+                                                <div style={{ color: 'red' }}>⚠️</div>
+                                            </Tooltip>
+                                        )} </div>))
                             }
                         </Stack>
                         <Stack direction={'row'} >
@@ -40,7 +53,7 @@ const DocumentNumbering = () => {
                                 editDocumentInputs1(document).map(item => <FormInput input={item as IInput} changeState={onChangeInputs} error={false} readonly={false} />)
                             }
                         </Stack>
-                        <Stack direction={'row'} display={"Flex"} justifyContent={"flex-end"} paddingTop={7}>
+                        <Stack direction={'row'} display={"Flex"} justifyContent={"flex-end"} paddingTop={6.5}>
                             <SecondaryButton style={classes.btnStyle} onClick={() => onUpdateDocument(document)} variant="contained">{t('documentingSettings.update')}</SecondaryButton>
                         </Stack>
                     </Stack>
