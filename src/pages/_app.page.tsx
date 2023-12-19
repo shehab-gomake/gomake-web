3c569702f2608672c55e0660dd5ad68ad16b81ec
lamaAnbtawe <106581571+lamaAnbtawe@users.noreply.github.com>
2023-12-19 16:51:23 +0200

    last fix

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -3,24 +3,19 @@ import type { AppProps } from "next/app";
 import { RecoilRoot } from "recoil";
 import i18n from "i18next";
 import { initReactI18next, useTranslation } from "react-i18next";
-import ar from "@/languages/ar.json";
-import en from "@/languages/en.json";
-import he from "@/languages/he.json";
-import de from "@/languages/de.json";
 import { ThemeProvider } from "@/providers";
 import { GomakeLoading } from "@/widgets";
 import { GoMakeSnackBar } from "@/components";
-import {DndProvider} from "react-dnd";
-import {HTML5Backend} from "react-dnd-html5-backend";
-import Backend from 'i18next-http-backend'; 
-const timestamp = new Date().getTime();
+import { DndProvider } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
+import Backend from 'i18next-http-backend';
 
 i18n
   .use(Backend)
   .use(initReactI18next)
   .init({
     backend: {
-      loadPath: `https://gomake-translations.s3.eu-central-1.amazonaws.com/{{lng}}.json?timestamp=${timestamp}`,
+      loadPath: `https://gomake-translations.s3.eu-central-1.amazonaws.com/{{lng}}.json`,
     },
     lng: "en",
     fallbackLng: "en",
@@ -34,18 +29,18 @@ export default function App({ Component, pageProps }: AppProps) {
   const { t } = useTranslation();
 
   return (
-      <DndProvider backend={HTML5Backend}>
+    <DndProvider backend={HTML5Backend}>
       <RecoilRoot>
-      <ThemeProvider />
-      <style jsx global>{`
+        <ThemeProvider />
+        <style jsx global>{`
         html {
           direction: ${t("direction")};
         }
       `}</style>
-      <Component {...pageProps} />
-      <GoMakeSnackBar />
-      <GomakeLoading />
-    </RecoilRoot>
-      </DndProvider>
+        <Component {...pageProps} />
+        <GoMakeSnackBar />
+        <GomakeLoading />
+      </RecoilRoot>
+    </DndProvider>
   );
 }
