4e49be7cbb7dc72ef64a40fa1bb888bda64d744c
lamaAnbtawe <106581571+lamaAnbtawe@users.noreply.github.com>
2023-12-20 16:20:03 +0200

    updates

--- src/widgets/materials-widget/components/table-cell-data/multi-select-input.tsx
+++ src/widgets/materials-widget/components/table-cell-data/multi-select-input.tsx
@@ -1,11 +1,12 @@
-import React, { useState } from "react";
+import React from "react";
 import { useRecoilValue } from "recoil";
 import { materialsMachinesState } from "@/widgets/materials-widget/state";
 import { useTableCellData } from "@/widgets/materials-widget/components/table-cell-data/use-table-cell-data";
-import { multiSelectInput } from "./inputs";
-import { FormInput } from "@/components/form-inputs/form-input";
-import { IInput } from "@/components/form-inputs/interfaces";
+import { GoMakeAutoComplate } from "@/components";
 import { Stack } from "@mui/material";
+import { Checkbox, Paper } from "@mui/material";
+import { CheckboxCheckedIcon, CheckboxIcon } from "@/icons";
+import { useStyle } from "./style";
 
 interface IProps {
     parameterKey: string;
@@ -14,21 +15,54 @@ interface IProps {
 }
 
 const MultiSelectInput = ({ values, parameterKey, id }: IProps) => {
+    const { classes } = useStyle();
     const { updateCellData } = useTableCellData();
     const machinesCategories = useRecoilValue<any>(materialsMachinesState);
-    const [state, setState] = useState<any>({});
 
-    const onChangeInputs = (key, value) => {
-        setState({ ...state, [key]: value })
-        updateCellData(id, 'machines', value);
+    const onChangeInputs = async (key, value) => {
+        await updateCellData(id, key , value);
     }
 
+    const options = machinesCategories.map(machine => ({
+        value: machine.id,
+        label: `${machine.manufacturer} - ${machine.model}`
+    }));
+
+    const selectedLabels = values.map(selectedId => options.find(opt => opt.value === selectedId)?.label)
+        .filter(label => label !== null && label !== undefined);
+
+    const handleSelectCheck = (parameterKey: string, isChecked: boolean, option: any) => {
+        onChangeInputs(parameterKey, isChecked ? [...values, option?.value] : values.filter(v => v !== option?.value));
+    };
+
     return (
-        <>
-            {
-                multiSelectInput(state, machinesCategories, values).map(item => <FormInput input={item as IInput} changeState={onChangeInputs} error={false} readonly={false} />)
-            }
-        </>
+        <div style={{padding:"0px 20px 0px 20px"}}>
+            <GoMakeAutoComplate
+                style={{ border: 0 }}
+                //onChange={onChangeInputs}
+                value={selectedLabels}
+                disabled={false}
+                placeholder={"Select machines"}
+                options={options}
+                renderOption={(props: any, option: any) => {
+                    return (
+                        <Stack style={classes.multiSelectOption}>
+                            <div>
+                                <Checkbox
+                                    onChange={(e, checked) => handleSelectCheck(parameterKey, checked, option)}
+                                    icon={<CheckboxIcon />}
+                                    checkedIcon={<CheckboxCheckedIcon />}
+                                    checked={values?.includes(option?.value)} />
+                            </div>
+                            <div>
+                                {option.label}
+                            </div>
+                        </Stack>
+                    )
+                }}
+                PaperComponent={(props: any) => <Paper style={{ width: "180px" }} {...props}>{props?.children}</Paper>}
+            />
+        </div>
     )
 }
 
