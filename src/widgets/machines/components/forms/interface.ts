b21cabdf96204e3ef0e5456b01b1280f03c714c7
MohnadGomake <95920049+MohnadGomake@users.noreply.github.com>
2023-12-23 14:54:55 +0200

    Update interface.ts

--- src/widgets/machines/components/forms/interface.ts
+++ src/widgets/machines/components/forms/interface.ts
@@ -1,3 +1,11 @@
 export interface IStepFormProps {
     children?: JSX.Element
+    navigateBack:()=>void;
+    navigateNext:()=>void;
+    canAddMachine:boolean;
+    canUpdate:boolean;
+    onClickAdd:()=>void;
+    onClickUpdate:()=>void;
+    hasBack:boolean;
+    hasNext:boolean;
 }
