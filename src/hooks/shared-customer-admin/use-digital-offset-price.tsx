a77a1d41e3b52894b2e2de278dbc4695f36dd09c
MohnadGomake <95920049+MohnadGomake@users.noreply.github.com>
2023-12-25 23:57:54 +0200

    chnages

--- src/hooks/shared-customer-admin/use-digital-offset-price.tsx
+++ src/hooks/shared-customer-admin/use-digital-offset-price.tsx
@@ -103,6 +103,7 @@ const useDigitalOffsetPrice = ({ clasess, widgetType }) => {
     useRecoilState<any>(digitslPriceState);
   const [priceRecovery, setPriceRecovery] = useState(true);
   const [canCalculation, setCanCalculation] = useState(false);
+  const [isCalculationFinished, setIsCalculationFinished] = useState(false);
   const setSelectParameterButton = useSetRecoilState(
     selectParameterButtonState
   );
@@ -147,7 +148,9 @@ const useDigitalOffsetPrice = ({ clasess, widgetType }) => {
         currentWorkFlows.sort((a,b) => b.monials - a.monials );
         const currentWorkFlowsCount = currentWorkFlows.length;
         const totalWorkFlowsCount = calculationResult?.pricingDto.totalWorkFlows;
-        setCalculationProgress({totalWorkFlowsCount: totalWorkFlowsCount,currentWorkFlowsCount:currentWorkFlowsCount} );
+        if(!isCalculationFinished){
+          setCalculationProgress({totalWorkFlowsCount: totalWorkFlowsCount,currentWorkFlowsCount:currentWorkFlowsCount} );
+        }
         setWorkFlows(currentWorkFlows);
         setJobActions(calculationResult?.pricingDto.actions);
       }
@@ -1081,7 +1084,6 @@ const useDigitalOffsetPrice = ({ clasess, widgetType }) => {
         const subSection = section?.subSections?.find(x=>x.id === itemParmetersValue?.subSectionId);
         let parameter = subSection.parameters.find(x=>x.id === itemParmetersValue.parameterId)
         if(!parameter && subSection.parameters && subSection.parameters.length > 0){
-          debugger;
           const  parentParameter = subSection.parameters.find(x=>x.settingParameters?.some(y=>y.id === itemParmetersValue.parameterId));
           parameter = parentParameter?.settingParameters?.find(x=>x.id === itemParmetersValue.parameterId);
         }
@@ -1147,6 +1149,7 @@ const useDigitalOffsetPrice = ({ clasess, widgetType }) => {
     }
     setWorkFlows([]);
     setJobActions([]);
+    setIsCalculationFinished(false)
     setCalculationProgress({
       totalWorkFlowsCount: 0,
       currentWorkFlowsCount: 0,
@@ -1174,15 +1177,29 @@ const useDigitalOffsetPrice = ({ clasess, widgetType }) => {
         false,
         newRequestAbortController
       );
+      setIsCalculationFinished(true)
       if (res?.success) {
-        // setPricingDefaultValue(res?.data?.data?.data);
-        /*setWorkFlows(
-                    res?.data?.data?.data?.workFlows?.map((flow, index) => ({
-                        id: index.toString(),
-                        ...flow,
-                    }))
-                );
-                setJobActions(res?.data?.data?.data?.actions);*/
+         setPricingDefaultValue(res?.data?.data?.data);
+         const workFlows = res?.data?.data?.data?.workFlows;
+         if(workFlows && workFlows.length > 0){
+           const workFlow = res?.data?.data?.data?.workFlows.find(x=>x.selected);
+           const currentWorkFlows = cloneDeep(workFlows);
+           const isExits = currentWorkFlows.find(x=>x.id === workFlow.id);
+           if(!isExits){
+             currentWorkFlows.push(workFlow)
+           }
+           if(isExits && !isExits.selected) {
+             currentWorkFlows.forEach(x => x.selected = false);
+             isExits.selected = true;
+           }
+           currentWorkFlows.sort((a,b) => b.monials - a.monials );
+           setCalculationProgress({totalWorkFlowsCount: 0,currentWorkFlowsCount:0} );
+           setWorkFlows(currentWorkFlows);
+         }
+       
+         
+         
+          setJobActions(res?.data?.data?.data?.actions);
       }
       setLoading(false);
     }
