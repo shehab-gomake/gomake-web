458715adff9bd3950732ec84f394b0236a5a7d88
riadFadila <riad@gomake.net>
2023-12-26 22:33:17 +0200

    select machine for workflow action

--- src/services/api-service/quotes/save-product-item-value-draft-api.ts
+++ src/services/api-service/quotes/save-product-item-value-draft-api.ts
@@ -2,8 +2,12 @@ import {ICallAndSetData} from "@/services/api-service/interface";
 import {getSetApiData} from "@/services/api-service/get-set-api-data";
 import {EHttpMethod} from "@/services/api-service/enums";
 const SAVE_PRODUCT_ITEM_VALUE_URL = '/v1/erp-service/quote/save-product-item-draft';
+const UPDATE_PRODUCT_ITEM_ACTION_MACHINE_URL = '/v1/erp-service/quote/update-product-item-draft-action-machine';
 
 const saveProductItemValueDraft: ICallAndSetData = async (callApi, setState, data: {isAgent: boolean}) => {
     return await getSetApiData(callApi, EHttpMethod.POST, SAVE_PRODUCT_ITEM_VALUE_URL, setState , data);
 }
-export { saveProductItemValueDraft };
+const updateProductItemDraftActionMachine: ICallAndSetData = async (callApi, setState, data:{}) => {
+    return await getSetApiData(callApi, EHttpMethod.POST, UPDATE_PRODUCT_ITEM_ACTION_MACHINE_URL, setState , data);
+}
+export { saveProductItemValueDraft, updateProductItemDraftActionMachine };
