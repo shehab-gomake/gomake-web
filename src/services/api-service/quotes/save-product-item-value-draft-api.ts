9f6ee24d5a86c9d9b9312bcb413bdf2217ab1d28
lamaAnbtawe <106581571+lamaAnbtawe@users.noreply.github.com>
2023-12-31 15:27:14 +0200

    deleting unused files

--- src/services/api-service/quotes/save-product-item-value-draft-api.ts
+++
@@ -1,9 +0,0 @@
-import {ICallAndSetData} from "@/services/api-service/interface";
-import {getSetApiData} from "@/services/api-service/get-set-api-data";
-import {EHttpMethod} from "@/services/api-service/enums";
-const SAVE_PRODUCT_ITEM_VALUE_URL = '/v1/erp-service/quote/save-product-item-draft';
-
-const saveProductItemValueDraft: ICallAndSetData = async (callApi, setState, data: {isAgent: boolean}) => {
-    return await getSetApiData(callApi, EHttpMethod.POST, SAVE_PRODUCT_ITEM_VALUE_URL, setState , data);
-}
-export { saveProductItemValueDraft };
