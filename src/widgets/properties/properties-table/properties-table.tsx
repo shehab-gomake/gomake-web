2e13ca0fdea5ac012200129c10bd800aea461bc2
Ahmed <shehab@gomake.net>
2024-01-01 11:41:43 +0200

    fix propaterties table

--- src/widgets/properties/properties-table/properties-table.tsx
+++ src/widgets/properties/properties-table/properties-table.tsx
@@ -1,50 +1,13 @@
-import { styled } from "@mui/material/styles";
-import {
-  IconButton,
-  Table,
-  TableBody,
-  TableCell,
-  tableCellClasses,
-  TableContainer,
-  TableHead,
-  TableRow,
-} from "@mui/material";
-import { FONT_FAMILY } from "@/utils/font-family";
-import { useStyle } from "@/widgets/properties/properties-table/style";
-import { useCallback, useState } from "react";
 import { useTranslation } from "react-i18next";
-import { MoreMenuWidget } from "../more-circle";
-import { HeaderTitleWithSearch } from "@/widgets/header-title-with-search";
 
-import { useProperites } from "../hooks/use-properites";
-import { MoreCircleIcon } from "@/icons";
 import { AddRuleModal } from "@/pages-components/products/profits-new/widgets/add-rule-modal";
-import { EditRulesModal } from "../properties-modals/edit-rule-modal";
-import { useRouter } from "next/router";
+import { HeaderTitleWithSearch } from "@/widgets/header-title-with-search";
+import { useStyle } from "@/widgets/properties/properties-table/style";
 import { PrimaryTable } from "@/components/tables/primary-table";
 
-const StyledTableCell = styled(TableCell)(() => ({
-  [`&.${tableCellClasses.head}`]: {
-    backgroundColor: "#EBECFF",
-    color: "#000000",
-    ...FONT_FAMILY.Lexend(500, 14),
-    padding: 5,
-  },
-  [`&.${tableCellClasses.body}`]: {
-    ...FONT_FAMILY.Lexend(500, 14),
-    color: "#656572",
-    padding: 5,
-  },
-}));
-
-const StyledTableRow = styled(TableRow)(() => ({
-  "&:nth-of-type(even)": {
-    backgroundColor: "#ECECEC",
-  },
-  "&:last-child td, &:last-child th": {
-    border: 0,
-  },
-}));
+import { EditRulesModal } from "../properties-modals/edit-rule-modal";
+import { useProperites } from "../hooks/use-properites";
+import { MoreMenuWidget } from "../more-circle";
 
 const PropertiesTable = () => {
   const { t } = useTranslation();
@@ -60,20 +23,17 @@ const PropertiesTable = () => {
     open,
     handleClose,
     selectedProperties,
-    allProperties,
-    setFilter,
+    propertiesData,
+    tableHeaders,
+    router,
+    term,
+    propertiesDataSearched,
     getProperitesService,
     deleteRule,
     reOrderPricingTables,
+    setTerm,
   } = useProperites({ classes });
-  const router = useRouter();
-  const tableHeaders = [
-    t("properties.parameter"),
-    t("properties.unit"),
-    t("properties.rule"),
-    t("properties.type"),
-    t("properties.more"),
-  ];
+
   return (
     <div style={classes.mainContainer}>
       <div style={classes.headerContainer}>
@@ -81,87 +41,30 @@ const PropertiesTable = () => {
           title={
             router.query.actionName + " " + t("products.actions.properties")
           }
-          onChange={(e) => setFilter(e)}
+          onChange={(e) => setTerm(e)}
         />
       </div>
-      <PrimaryTable
+      {term ? (
+        <PrimaryTable
+          stickyFirstCol={false}
+          stickyHeader={false}
+          rows={propertiesDataSearched}
+          headers={tableHeaders}
+        />
+      ) : (
+        <PrimaryTable
+          stickyFirstCol={false}
+          stickyHeader={false}
+          rows={propertiesData}
+          headers={tableHeaders}
+        />
+      )}
+      {/* <PrimaryTable
         stickyFirstCol={false}
         stickyHeader={false}
-        rows={allProperties}
+        rows={propertiesData}
         headers={tableHeaders}
-      />
-      {/* <TableContainer style={classes.shadowBorder}>
-        <Table>
-          <TableHead>
-            <TableRow style={{ background: "#EBECFF" }}>
-              <StyledTableCell align={"center"}>
-                {t("properties.parameter")}
-              </StyledTableCell>
-              <StyledTableCell align={"center"}>
-                {t("properties.unit")}
-              </StyledTableCell>
-              <StyledTableCell style={{ width: "30%" }} align={"center"}>
-                {t("properties.rule")}
-              </StyledTableCell>
-              <StyledTableCell align={"center"}>
-                {t("properties.type")}
-              </StyledTableCell>
-              <StyledTableCell align={"center"}>
-                {t("properties.more")}
-              </StyledTableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {properties()?.map((property, index) => {
-              return (
-                <StyledTableRow>
-                  <StyledTableCell align={"center"}>
-                    {property.propertyName}{" "}
-                  </StyledTableCell>
-                  <StyledTableCell align={"center"}>
-                    {property.defaultUnit}
-                  </StyledTableCell>
-                  <StyledTableCell align={"center"}>
-                    <div style={classes.rowItem} className="scrollBlue">
-                      {property.actionRules
-                        ?.sort((a, b) => a.priority - b.priority)
-                        ?.map((rule, index) => {
-                          return (
-                            <div style={classes.item}>
-                              {rule.successEvent ? (
-                                <>
-                                  {index + 1}- {rule.expression} value=
-                                  {rule.successEvent}
-                                </>
-                              ) : (
-                                <>
-                                  {index + 1}- {rule.expression}
-                                </>
-                              )}
-                            </div>
-                          );
-                        })}
-                    </div>
-                  </StyledTableCell>
-                  <StyledTableCell align={"center"}>
-                    {property.ruleType == 0 ? "Output" : "Input"}{" "}
-                  </StyledTableCell>
-                  <StyledTableCell align={"center"}>
-                    <IconButton
-                      onClick={(e) => {
-                        handleClick(e);
-                        setSelectedProperites(property);
-                      }}
-                    >
-                      <MoreCircleIcon />
-                    </IconButton>
-                  </StyledTableCell>
-                </StyledTableRow>
-              );
-            })}
-          </TableBody>
-        </Table>
-      </TableContainer> */}
+      /> */}
       <AddRuleModal
         openModal={openAddRule}
         onCloseModal={onCloseAddRuleModal}
