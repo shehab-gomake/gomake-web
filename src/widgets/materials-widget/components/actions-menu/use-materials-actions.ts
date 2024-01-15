aff53ae4cdb5f2c6ff3bb5835ba03256ba1eec00
Ahmed <shehab@gomake.net>
2024-01-15 14:40:21 +0200

    fix close modal bug

--- src/widgets/materials-widget/components/actions-menu/use-materials-actions.ts
+++ src/widgets/materials-widget/components/actions-menu/use-materials-actions.ts
@@ -1,22 +1,28 @@
-import {useRecoilState, useRecoilValue, useSetRecoilState} from "recoil";
+import { useRecoilState, useRecoilValue, useSetRecoilState } from "recoil";
 import { IMaterialCategoryRow } from "@/widgets/materials-widget/interface";
 import {
-    activeFilterState, filterState,
-    isAllMaterialsCheckedState,
-    materialCategoryDataState, materialsUnCheckedState,
-    openAddRowModalState, selectedSupplierIdState
+  activeFilterState,
+  filterState,
+  isAllMaterialsCheckedState,
+  materialCategoryDataState,
+  materialsUnCheckedState,
+  openAddRowModalState,
+  selectedSupplierIdState,
 } from "@/widgets/materials-widget/state";
-import {useEffect, useRef, useState} from "react";
-import { EMaterialActiveFilter,EMaterialsActions } from "@/widgets/materials-widget/enums";
+import { useEffect, useRef, useState } from "react";
+import {
+  EMaterialActiveFilter,
+  EMaterialsActions,
+} from "@/widgets/materials-widget/enums";
 import { useGomakeAxios, useSnackBar } from "@/hooks";
 import { useRouter } from "next/router";
 import { useTranslation } from "react-i18next";
-import {updatePrintHouseMaterialsPropApi} from "@/services/api-service/materials/printhouse-materials-endpoints";
+import { updatePrintHouseMaterialsPropApi } from "@/services/api-service/materials/printhouse-materials-endpoints";
 
 import {
-    getMaterialExcelFileApi,
-    updateMaterialsPropApi,
-    uploadMaterialExcelFileApi
+  getMaterialExcelFileApi,
+  updateMaterialsPropApi,
+  uploadMaterialExcelFileApi,
 } from "@/services/api-service/materials/materials-endpoints";
 import { useExchangeRate } from "@/hooks/use-exchange-rate";
 import { useMaterialsCategories } from "../../use-materials-categories";
@@ -36,13 +42,15 @@ const useMaterialsActions = (isAdmin: boolean) => {
   const [currentCurrency, setCurrentCurrency] = useState<any>("");
   const [checkedPrice, setCheckedPrice] = useState(false);
   const { rate, setRate, getExchangeRate } = useExchangeRate();
-  const isAllMaterialsChecked = useRecoilValue<boolean>(isAllMaterialsCheckedState);
+  const isAllMaterialsChecked = useRecoilValue<boolean>(
+    isAllMaterialsCheckedState
+  );
   const uncheckedMaterials = useRecoilValue<string[]>(materialsUnCheckedState);
   const supplierId = useRecoilValue(selectedSupplierIdState);
   const activeFilter = useRecoilValue(activeFilterState);
-    const materialFilter = useRecoilValue(filterState);
-    const elementRef = useRef(null);
-    const {getMaterialCategoryData} = useMaterialsCategories(isAdmin);
+  const materialFilter = useRecoilValue(filterState);
+  const elementRef = useRef(null);
+  const { getMaterialCategoryData } = useMaterialsCategories(isAdmin);
   useEffect(() => {
     if (checkedPrice) getExchangeRate(currentCurrency, updatedValue);
   }, [checkedPrice, updatedValue, currentCurrency]);
@@ -54,17 +62,17 @@ const useMaterialsActions = (isAdmin: boolean) => {
   const onChooseAction = async (
     action: { action: EMaterialsActions; key: string } | null
   ) => {
-    if(action.action === EMaterialsActions.AddNew){
-        setOpenAddRowModal(true);
-        return;
+    if (action?.action === EMaterialsActions.AddNew) {
+      setOpenAddRowModal(true);
+      return;
     }
-    if(action.action === EMaterialsActions.DownLoadExcel){
-        downloadExcelFile().then();
-        return;
+    if (action?.action === EMaterialsActions.DownLoadExcel) {
+      downloadExcelFile().then();
+      return;
     }
-    if(action.action === EMaterialsActions.UploadExcel){
-        elementRef.current.click()
-        return;
+    if (action?.action === EMaterialsActions.UploadExcel) {
+      elementRef.current.click();
+      return;
     }
     if (getSelectedMaterialsIds().length > 0) {
       const myId = getSelectedMaterialsIds()[0];
@@ -107,22 +115,22 @@ const useMaterialsActions = (isAdmin: boolean) => {
           ids: getSelectedMaterialsIds(),
           action: action.action,
           priceIndex: 0,
-            isAllMaterialsChecked:isAllMaterialsChecked,
-            uncheckedMaterials:uncheckedMaterials,
-            tableFilters:{
-                materialKey: materialType,
-                categoryKey: materialCategory,
-                supplierId,
-                pageNumber:null,
-                pageSize:null,
-                isActive:
-                    activeFilter === EMaterialActiveFilter.ACTIVE
-                        ? true
-                        : activeFilter === EMaterialActiveFilter.INACTIVE
-                            ? false
-                            : null,
-                customFiltersKeyValueList:materialFilter,
-            },
+          isAllMaterialsChecked: isAllMaterialsChecked,
+          uncheckedMaterials: uncheckedMaterials,
+          tableFilters: {
+            materialKey: materialType,
+            categoryKey: materialCategory,
+            supplierId,
+            pageNumber: null,
+            pageSize: null,
+            isActive:
+              activeFilter === EMaterialActiveFilter.ACTIVE
+                ? true
+                : activeFilter === EMaterialActiveFilter.INACTIVE
+                ? false
+                : null,
+            customFiltersKeyValueList: materialFilter,
+          },
           exchangeRate: rate,
         });
       } else {
@@ -133,22 +141,22 @@ const useMaterialsActions = (isAdmin: boolean) => {
           action: action.action,
           priceIndex: 0,
           updatedValue,
-            isAllMaterialsChecked:isAllMaterialsChecked,
-            uncheckedMaterials:uncheckedMaterials,
-            tableFilters:{
-                materialKey: materialType,
-                categoryKey: materialCategory,
-                supplierId,
-                pageNumber:null,
-                pageSize:null,
-                isActive:
-                    activeFilter === EMaterialActiveFilter.ACTIVE
-                        ? true
-                        : activeFilter === EMaterialActiveFilter.INACTIVE
-                            ? false
-                            : null,
-                customFiltersKeyValueList:materialFilter,
-            },
+          isAllMaterialsChecked: isAllMaterialsChecked,
+          uncheckedMaterials: uncheckedMaterials,
+          tableFilters: {
+            materialKey: materialType,
+            categoryKey: materialCategory,
+            supplierId,
+            pageNumber: null,
+            pageSize: null,
+            isActive:
+              activeFilter === EMaterialActiveFilter.ACTIVE
+                ? true
+                : activeFilter === EMaterialActiveFilter.INACTIVE
+                ? false
+                : null,
+            customFiltersKeyValueList: materialFilter,
+          },
           exchangeRate: rate,
         });
       }
@@ -156,125 +164,138 @@ const useMaterialsActions = (isAdmin: boolean) => {
   };
 
   const onUpdateCallBack = (res) => {
-      if (res.success) {
-          setMaterialCategoryData(
-              materialCategoryData.map((material) =>
-                  material.checked
-                      ? {
-                          ...material,
-                          ...res.data?.find((row) => row.id === material.id),
-                      }
-                      : material
-              )
-          );
+    if (res.success) {
+      setMaterialCategoryData(
+        materialCategoryData.map((material) =>
+          material.checked
+            ? {
+                ...material,
+                ...res.data?.find((row) => row.id === material.id),
+              }
+            : material
+        )
+      );
+    }
+  };
+  const updateStatus = async (eAction: EMaterialsActions) => {
+    if (isAdmin) {
+      const result = await updateMaterialsPropApi(callApi, onUpdateCallBack, {
+        materialTypeKey: materialType.toString(),
+        categoryKey: materialCategory.toString(),
+        ids: getSelectedMaterialsIds(),
+        action: eAction,
+        isAllMaterialsChecked: isAllMaterialsChecked,
+        uncheckedMaterials: uncheckedMaterials,
+        tableFilters: {
+          materialKey: materialType,
+          categoryKey: materialCategory,
+          supplierId,
+          pageNumber: null,
+          pageSize: null,
+          isActive:
+            activeFilter === EMaterialActiveFilter.ACTIVE
+              ? true
+              : activeFilter === EMaterialActiveFilter.INACTIVE
+              ? false
+              : null,
+          customFiltersKeyValueList: materialFilter,
+        },
+        priceIndex: 0,
+      });
+      if (result?.success) {
+        getMaterialCategoryData(
+          materialType?.toString(),
+          materialCategory?.toString(),
+          [],
+          supplierId
+        ).then();
       }
-  }
-    const updateStatus = async (eAction: EMaterialsActions) => {
-        if(isAdmin){
-          const result = await updateMaterialsPropApi(callApi, onUpdateCallBack, {
-                materialTypeKey: materialType.toString(),
-                categoryKey: materialCategory.toString(),
-                ids: getSelectedMaterialsIds(),
-                action: eAction,
-                isAllMaterialsChecked:isAllMaterialsChecked,
-                uncheckedMaterials:uncheckedMaterials,
-                tableFilters:{
-                    materialKey: materialType,
-                    categoryKey: materialCategory,
-                    supplierId,
-                    pageNumber:null,
-                    pageSize:null,
-                    isActive:
-                        activeFilter === EMaterialActiveFilter.ACTIVE
-                            ? true
-                            : activeFilter === EMaterialActiveFilter.INACTIVE
-                                ? false
-                                : null,
-                    customFiltersKeyValueList:materialFilter,
-                },
-                priceIndex: 0,
-            })
-            if(result?.success){
-                getMaterialCategoryData(materialType?.toString(), materialCategory?.toString(),[], supplierId).then();
-            }
-        }else{
-            const result = await updatePrintHouseMaterialsPropApi(callApi, onUpdateCallBack, {
-                materialTypeKey: materialType.toString(),
-                categoryKey: materialCategory.toString(),
-                ids: getSelectedMaterialsIds(),
-                action: eAction,
-                isAllMaterialsChecked:isAllMaterialsChecked,
-                uncheckedMaterials:uncheckedMaterials,
-                tableFilters:{
-                    materialKey: materialType,
-                    categoryKey: materialCategory,
-                    supplierId,
-                    isActive:
-                        activeFilter === EMaterialActiveFilter.ACTIVE
-                            ? true
-                            : activeFilter === EMaterialActiveFilter.INACTIVE
-                                ? false
-                                : null,
-                    customFiltersKeyValueList:materialFilter,
-                },
-                priceIndex: 0,
-            })
-            if(result?.success){
-                getMaterialCategoryData(materialType?.toString(), materialCategory?.toString(),[], supplierId).then();
-            }
+    } else {
+      const result = await updatePrintHouseMaterialsPropApi(
+        callApi,
+        onUpdateCallBack,
+        {
+          materialTypeKey: materialType.toString(),
+          categoryKey: materialCategory.toString(),
+          ids: getSelectedMaterialsIds(),
+          action: eAction,
+          isAllMaterialsChecked: isAllMaterialsChecked,
+          uncheckedMaterials: uncheckedMaterials,
+          tableFilters: {
+            materialKey: materialType,
+            categoryKey: materialCategory,
+            supplierId,
+            isActive:
+              activeFilter === EMaterialActiveFilter.ACTIVE
+                ? true
+                : activeFilter === EMaterialActiveFilter.INACTIVE
+                ? false
+                : null,
+            customFiltersKeyValueList: materialFilter,
+          },
+          priceIndex: 0,
         }
-
+      );
+      if (result?.success) {
+        getMaterialCategoryData(
+          materialType?.toString(),
+          materialCategory?.toString(),
+          [],
+          supplierId
+        ).then();
+      }
     }
+  };
 
-    const downloadExcelFile = async () => {
-        const callBack = (res) => {
-            if (res.success) {
-                const downloadLink = document.createElement("a");
-                downloadLink.href = res.data; // Use the provided file URL
-                downloadLink.download = materialType + ".xlsx"; // Replace with the desired file name
-                downloadLink.click();
-            }
-        };
-        await getMaterialExcelFileApi(callApi, callBack, materialType);
+  const downloadExcelFile = async () => {
+    const callBack = (res) => {
+      if (res.success) {
+        const downloadLink = document.createElement("a");
+        downloadLink.href = res.data; // Use the provided file URL
+        downloadLink.download = materialType + ".xlsx"; // Replace with the desired file name
+        downloadLink.click();
+      }
     };
+    await getMaterialExcelFileApi(callApi, callBack, materialType);
+  };
 
-    const uploadExcelFile = async (e) => {
-        const file = e.target.files[0];
-        if (file) {
-            const reader = new FileReader();
-            reader.onload = (event) => {
-                const arrayBuffer = event.target.result;
-                const data = new Uint8Array(arrayBuffer as ArrayBuffer);
-                // Convert data to a Base64 string
-                var base64String = btoa(
-                    new Uint8Array(data).reduce(function (data, byte) {
-                        return data + String.fromCharCode(byte);
-                    }, "")
-                );
-                uploadMaterialExcelFileApi(callApi, () => {}, {
-                    key: materialType.toString(),
-                    base64: base64String,
-                });
-            };
-            reader.readAsArrayBuffer(file);
-        }
-    };
- 
-    return {
-        getSelectedMaterialsIds,
-        onChooseAction,
-        action,
-        updatedValue,
-        onTextInputChange,
-        onInputChange,
-        uploadExcelFile,
-        onUpdate,
-        checkedPrice,
-        setCheckedPrice,
-        setRate,
-        rate,
-        elementRef
+  const uploadExcelFile = async (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = (event) => {
+        const arrayBuffer = event.target.result;
+        const data = new Uint8Array(arrayBuffer as ArrayBuffer);
+        // Convert data to a Base64 string
+        var base64String = btoa(
+          new Uint8Array(data).reduce(function (data, byte) {
+            return data + String.fromCharCode(byte);
+          }, "")
+        );
+        uploadMaterialExcelFileApi(callApi, () => {}, {
+          key: materialType.toString(),
+          base64: base64String,
+        });
+      };
+      reader.readAsArrayBuffer(file);
     }
-}
+  };
+
+  return {
+    getSelectedMaterialsIds,
+    onChooseAction,
+    action,
+    updatedValue,
+    onTextInputChange,
+    onInputChange,
+    uploadExcelFile,
+    onUpdate,
+    checkedPrice,
+    setCheckedPrice,
+    setRate,
+    rate,
+    elementRef,
+  };
+};
 
 export { useMaterialsActions };
