cfe94d1a29a37de120580a4e43b02af7ca2a3be8
shehab-gomake <129649590+shehab-gomake@users.noreply.github.com>
2024-02-01 18:41:54 +0200

    fix

--- src/hooks/shared-customer-admin/use-digital-offset-price.tsx
+++ src/hooks/shared-customer-admin/use-digital-offset-price.tsx
@@ -1188,21 +1188,6 @@ const useDigitalOffsetPrice = ({ clasess, widgetType }) => {
                         p.actionIndex === relatedParameter.actionIndex
                     );
                     if (relatedParameter.activateByAllValues && parm?.values) {
-                      /*return (
-                        <div>
-                          {_renderParameterType(
-                            myParameter,
-                            subSection,
-                            section,
-                            subSection?.parameters,
-                            myParameter?.value,
-                            list,
-                            true,
-                            false
-                          )}
-                        </div>
-                      );*/
-
                       let productCopy = cloneDeep(productTemplate);
                       const sectionCopy = productCopy.sections.find(x => x.id === section.id);
                       const subSectionCopy = sectionCopy.subSections.find(x => x.id === subSection.id);
