85ea0b8c969aa3db299811cd89aa18e5d0d895e9
lamaAnbtawe <106581571+lamaAnbtawe@users.noreply.github.com>
2024-02-01 11:40:35 +0200

    new updates

--- src/pages-components/quotes/statistics-section/card.tsx
+++ src/pages-components/quotes/statistics-section/card.tsx
@@ -1,6 +1,6 @@
 import { Card, CardContent, Typography } from "@mui/material";
 import { useStyle } from "./style";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 interface ICardProps {
     textColor?: string;
@@ -9,31 +9,44 @@ interface ICardProps {
     icon?: JSX.Element;
     onClick?: () => void;
     onSecondClick?: () => void;
+    isActive?:any,
+    onActivate?:any;
 }
 
-const CardComponent = ({ text, textColor, number, icon, onClick, onSecondClick }: ICardProps) => {
+const CardComponent = ({ text, textColor, number, icon, onClick, onSecondClick , isActive, onActivate }: ICardProps) => {
     const { classes } = useStyle();
-    const [isFiltered, setIsFiltered] = useState(false);
+    const [isFiltered, setIsFiltered] = useState(isActive || false);
 
     const mergedStyles = {
         ...classes.ticketStyle,
-        cursor: onClick ? "pointer" : "auto",
+       cursor: onClick ? "pointer" : "auto",
        border: isFiltered ? "none" : `2px solid ${textColor}`,
        backgroundColor: isFiltered ? textColor : classes.ticketStyle.background ,
 
     };
 
     const handleClick = () => {
-        setIsFiltered((prev) => !prev);
+        setIsFiltered((prev) => !prev); 
+        if (onActivate) {
+            onActivate();
+            console.log(text )
+        }
         if (isFiltered && onSecondClick) {
+            console.log(text )
             onSecondClick();
         } else if (!isFiltered && onClick) {
+            console.log( text  )
             onClick();
         }
     };
 
+
+    useEffect(() => {
+        setIsFiltered(isActive || false);
+    }, [isActive]);
+
     return (
-        <Card sx={mergedStyles} onClick={onClick && handleClick}>
+        <Card sx={mergedStyles} onClick={ onClick && handleClick}>
             <CardContent style={classes.ticketContentStyle}>
                 <Typography sx={classes.textStyle} color={isFiltered ?"#FFFFFF" :textColor } >{icon}{text}</Typography>
                 <Typography sx={classes.numberStyle} color={isFiltered ? "#FFFFFF":textColor }>{number}</Typography>
--- src/pages-components/quotes/statistics-section/statistics-sections.tsx
+++ src/pages-components/quotes/statistics-section/statistics-sections.tsx
@@ -9,6 +9,7 @@ import DoneAllIcon from '@mui/icons-material/DoneAll';
 import DoDisturbAltIcon from '@mui/icons-material/DoDisturbAlt';
 import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
 import { GoMakeCurrency } from "@/icons/go-make-currency";
+import { useState } from "react";
 
 interface ICard {
     key: string;
@@ -19,6 +20,7 @@ interface ICardsSection {
     statistics: ICard[];
     onClick: () => void;
     setState?: any; 
+    state?:any;
 }
 
 const getValueByKey = (statistics: ICard[], key: string) => {
@@ -26,23 +28,42 @@ const getValueByKey = (statistics: ICard[], key: string) => {
     return card ? card.value : "";
 };
 
-const CardsSection = ({ statistics, onClick , setState}: ICardsSection) => {
+const CardsSection = ({ statistics , onClick }: ICardsSection) => {
     const { t } = useTranslation();
     const { classes } = useStyle();
     const { primaryColor, secondColor, warningColor, successColor, errorColor } = useGomakeTheme();
+    const [activeCard, setActiveCard] = useState(null);
 
 
-    
+    const handleCardClick = (cardKey) => {
+        setActiveCard(cardKey);
+        // setState(cardKey)
+        // or setActiveCard used to be recoil state or come in props
+    };
+
     return (
         <Stack direction={"row"} gap={"5px"} alignItems={"center"} flexWrap={"wrap-reverse"}>
-            <CardComponent text={t("sales.quote.waiting")} number={getValueByKey(statistics, "waitingCount")}  textColor={warningColor(300)} icon={<HourglassTopIcon />} onClick={()=>console.log("search 1")}  onSecondClick={()=>console.log("clear 1")} />
-            <CardComponent text={t("sales.quote.approved")} number={getValueByKey(statistics, "approvedCount")}  textColor={successColor(300)} icon={<DoneAllIcon/>} onClick={()=>console.log("search 2")}  onSecondClick={()=>console.log("clear 2")}/>
-            <CardComponent text={t("sales.quote.canceled")} number={getValueByKey(statistics, "canceledCount")} textColor={errorColor(300)} icon={<DoDisturbAltIcon />} onClick={()=>console.log("search 3")}  onSecondClick={()=>console.log("clear 3")}/>
+            <CardComponent
+            onClick={() => handleCardClick("card-waiting")}
+            isActive={activeCard === "card-waiting"}
+            onActivate={() => console.log(`activate card-waiting`)}
+            key={"card-waiting"} text={t("sales.quote.waiting")} number={getValueByKey(statistics, "waitingCount")}  textColor={warningColor(300)} icon={<HourglassTopIcon />}   onSecondClick={()=>console.log("inactive waiting")} />
+            <CardComponent 
+            onClick={() => handleCardClick("card-approved")}
+            isActive={activeCard === "card-approved"}
+            onActivate={() => console.log(`activate card-approved`)}
+            key={"card-approved"} text={t("sales.quote.approved")} number={getValueByKey(statistics, "approvedCount")}  textColor={successColor(300)} icon={<DoneAllIcon/>}  onSecondClick={()=>console.log("inactive approved")}/>
+            <CardComponent
+            onClick={() => handleCardClick("card-canceled")}
+            isActive={activeCard === "card-canceled"}
+            onActivate={() => console.log(`activate card-canceled`)}
+            key={"card-canceled"} text={t("sales.quote.canceled")} number={getValueByKey(statistics, "canceledCount")} textColor={errorColor(300)} icon={<DoDisturbAltIcon />} onSecondClick={()=>console.log("inactive canceled")}/>
             <div style={classes.verticalLine}/>
             <CardComponent text={t("sales.quote.successRate")} number={getValueByKey(statistics, "successRate") + "%"}  textColor={primaryColor(300)} icon={<TrendingUpIcon/>} />
             <CardComponent text={t("sales.quote.totalPrice")} number={getValueByKey(statistics, "totalPrice")}  textColor={secondColor(100)} icon={<GoMakeCurrency color={secondColor(100)}/>} />
             <Button style={classes.createNew} onClick={onClick} startIcon={<AddCircleOutlineIcon style={{ color: 'black'  , fontSize:"24px"}}/>}>{t("sales.quote.createNew")}</Button>
-        </Stack>
+    </Stack>
+
     );
 }
 
