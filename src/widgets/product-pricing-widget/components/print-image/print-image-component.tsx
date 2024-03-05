8f680a307f225e082f860ea356e20eb5ce7fa4c6
MohnadGomake <95920049+MohnadGomake@users.noreply.github.com>
2024-03-05 17:46:59 +0200

    Update print-image-component.tsx

--- src/widgets/product-pricing-widget/components/print-image/print-image-component.tsx
+++ src/widgets/product-pricing-widget/components/print-image/print-image-component.tsx
@@ -9,7 +9,6 @@ import DownloadRoundedIcon from '@mui/icons-material/DownloadRounded';
 const PrintImageComponent = ({materialLength, materialWidth, rectangles, name}: IOutput) => {
     const [open, setOpen] = useState<boolean>(false);
     const imageElement = useRef(null);
-    const divider = 1000;
     const handleDownloadClick = () => {
         const element = imageElement?.current;
         if (element) {
@@ -23,13 +22,19 @@ const PrintImageComponent = ({materialLength, materialWidth, rectangles, name}: 
             });
         }
     };
-    console.log(materialWidth+","+materialLength+","+JSON.stringify(rectangles))
+    const scaleX = (number)=>{
+        return number / 1000;
+    }
+    const scaleY = (number)=>{
+        return number / 1000;
+    }
+    console.log(materialWidth + "," + materialLength + "," + rectangles.length + "," + JSON.stringify(rectangles))
     return (
         <div>
             <PrimaryButton variant={'contained'} style={{fontSize: '15px', padding: '5px 15px'}} onClick={() => setOpen(true)}>{name}</PrimaryButton>
             <GoMakeModal openModal={open} onClose={() => setOpen(false)}
                          modalTitle={name}
-                         insideStyle={{width: `${((materialWidth/divider)) + 100}px`, height: 'fit-content'}}>
+                         insideStyle={{width: `${(scaleX(materialWidth)) + 100}px`, height: 'fit-content',maxHeight:'80%'}}>
                 <Stack>
                     <IconButton style={{alignSelf: 'end'}} onClick={handleDownloadClick}>
                         <DownloadRoundedIcon/>
@@ -37,8 +42,9 @@ const PrintImageComponent = ({materialLength, materialWidth, rectangles, name}: 
                     <Stack  justifyContent={'center'} alignItems={'center'}>
                        <div>
                            <div ref={imageElement} style={{
-                               width: `${(materialWidth/divider)}px`,
-                               height: `${(materialLength/divider)}px`,
+                               width: `${scaleX(materialWidth)}px`,
+                               height: `${scaleX(materialLength)}px`,
+                               maxHeight:'80%',
                                border: '1px solid black',
                                position: 'relative'
                            }}>
@@ -51,10 +57,10 @@ const PrintImageComponent = ({materialLength, materialWidth, rectangles, name}: 
                                        return(
                                            <div style={{
                                                position: 'absolute',
-                                               left: ((x/divider)),
-                                               top: ((y/divider)),
-                                               width: (width/divider),
-                                               height: (length/divider),
+                                               left: (scaleX(x)),
+                                               top: (scaleY(y)),
+                                               width: scaleX(width),
+                                               height: scaleY(length),
                                                backgroundColor: 'black',
                                            }}/>
                                        )
