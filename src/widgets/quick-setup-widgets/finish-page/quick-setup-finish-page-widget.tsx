04da732363b48de26016dbdbecce0176e9593b99
riadFadila <riad@gomake.net>
2024-03-06 17:39:20 +0200

    products fixes

--- src/widgets/quick-setup-widgets/finish-page/quick-setup-finish-page-widget.tsx
+++ src/widgets/quick-setup-widgets/finish-page/quick-setup-finish-page-widget.tsx
@@ -4,12 +4,12 @@ import {useStyle} from "@/widgets/quick-setup-widgets/finish-page/style";
 import Image from "next/image";
 import rocket from "@/widgets/login/right-side/rocket.png";
 import Button from "@mui/material/Button";
-import {useRouter} from "next/router";
+import {useQuickSetupFinish} from "@/widgets/quick-setup-widgets/finish-page/use-quick-setup-finish";
 
 const QuickSetupFinishPageWidget = () => {
   const {t} = useTranslation();
   const {classes} = useStyle();
-  const {push} = useRouter();
+  const {handleClick} = useQuickSetupFinish();
   return (
       <Stack style={classes.container} alignItems={'center'} gap={'50px'} width={'100vw'} height={'100vh'} justifyContent={'center'} >
           <Image src={rocket} alt="gomake" priority  width={135} height={217}/>
@@ -21,7 +21,7 @@ const QuickSetupFinishPageWidget = () => {
                   )
               }
           </Stack>
-          <Button onClick={() => push('/')} style={classes.button} variant={'contained'}>{t('signup.finishPageBtn')}</Button>
+          <Button onClick={handleClick} style={classes.button} variant={'contained'}>{t('signup.finishPageBtn')}</Button>
       </Stack>
   );
 };
