69756aac61732846bc6e0443df7fa1fda792f8af
shehab-gomake <129649590+shehab-gomake@users.noreply.github.com>
2024-03-11 10:53:21 +0200

    clear code

--- src/pages-components/transaction-journal-report/use-transaction-journal-report.tsx
+++ src/pages-components/transaction-journal-report/use-transaction-journal-report.tsx
@@ -6,7 +6,7 @@ import { EHttpMethod } from "@/services/api-service/enums";
 import { DocumentsTypeReportList } from "@/enums";
 
 const useTransactionJournalReport = () => {
-  const { alertFaultGetData, alertSuccessGetData, alertFault, alertSuccess } = useSnackBar();
+  const { alertFaultGetData, alertSuccessGetData } = useSnackBar();
   const { callApi } = useGomakeAxios();
   const { t } = useTranslation()
 
@@ -31,46 +31,12 @@ const useTransactionJournalReport = () => {
 
 
   const [resetDatePicker, setResetDatePicker] = useState<boolean>(false);
-  const [isExtended, setIsExtended] = useState<boolean>(false);
   const [showTable, setShowTable] = useState<boolean>(false);
   const [fromDate, setFromDate] = useState<Date>(new Date());
   const [toDate, setToDate] = useState<Date>(new Date());
   const [dataTable, setDataTable] = useState<any>([]);
-  const [selectedContactById, setSelectedContactById] = useState<any>();
-  const [isopenEmailModal, setIsOpenEmailModal] = useState<boolean>(false);
-  const [isopenAdjustmentsModal, setIsOpenAdjustmentsModal] = useState<boolean>(false);
-  const [isopenCreateTransactionModal, setIsCreateTransactionModal] = useState<boolean>(false);
-  const [clientPaymentsList, setClientPaymentsList] = useState<any>([]);
 
-  const onClickCloseCreateTransactionModal = () => {
-    setIsCreateTransactionModal(false)
-  };
-  const onClickOpenCreateTransactionModal = () => {
-    setIsCreateTransactionModal(true)
-  };
 
-  const onClickCloseEmailModal = () => {
-    setIsOpenEmailModal(false);
-  };
-  const onClickOpenEmailModal = () => {
-    // if (customer?.id) {
-    //   setIsOpenEmailModal(true);
-    // }
-    // else {
-    //   alertFault("reports.pleaseSelectCustomer");
-    // }
-  };
-  const onClickCloseAdjustmentsModal = () => {
-    setIsOpenAdjustmentsModal(false);
-  };
-  const onClickOpenAdjustmentsModal = () => {
-    // if (customer?.id) {
-    //   setIsOpenAdjustmentsModal(true);
-    // }
-    // else {
-    //   alertFault("reports.pleaseSelectCustomer");
-    // }
-  };
   function getDocumentTypeName(transType: number): string {
     switch (transType) {
       case DocumentsTypeReportList.Invoice:
@@ -95,17 +61,6 @@ const useTransactionJournalReport = () => {
         return "Unknown";
     }
   }
-  const onChangeUpdateClientContact = useCallback(
-    (filedName: string, value: any) => {
-      setSelectedContactById((prev) => {
-        return {
-          ...prev,
-          [filedName]: value,
-        };
-      });
-    },
-    [selectedContactById]
-  );
   const getTableDataRows = useCallback((dataTable) => {
     if (dataTable?.length) {
       return dataTable?.map((data) => [
@@ -141,20 +96,9 @@ const useTransactionJournalReport = () => {
     setFromDate(fromDate);
     setToDate(toDate);
   };
-  const onChangeIsExtended = () => {
-    setIsExtended(!isExtended)
-  }
-  const onClickCreateNewTransaction = () => {
-    onClickOpenCreateTransactionModal()
-  }
-  const onClickSendingTicketByEmail = () => {
-    onClickOpenEmailModal()
-  }
+
   const onClickPrintCard = () => {
     ExportLedgerReportPDF()
-  }
-  const onClickAdjustments = () => {
-    onClickOpenAdjustmentsModal()
   }
   const onClickShowCard = () => {
     setShowTable(false);
@@ -170,21 +114,18 @@ const useTransactionJournalReport = () => {
           // "clientId": customer?.id,
           "startDate": fromDate,
           "endDate": toDate,
-          "isExtended": isExtended
         },
         true,
         null,
         "blob"
       );
-
-
       const downloadLink = document.createElement('a');
       const link = URL?.createObjectURL(res.data);
       downloadLink.href = link
       downloadLink.download = `aging report.pdf`;
       downloadLink.click();
     },
-    [fromDate, toDate, isExtended]
+    [fromDate, toDate]
   );
 
   const getTransactionJournalReportFilter = useCallback(
@@ -209,85 +150,17 @@ const useTransactionJournalReport = () => {
     },
     [fromDate, toDate, documentType,]
   );
-
-  const getClientPaymentItems = useCallback(
-    async () => {
-      const res = await callApi(
-        EHttpMethod.GET,
-        `/v1/erp-service/receipts/get-client-payment-items`,
-        {
-          // clientId: customer?.id,
-        }
-      );
-      if (res?.success) {
-        let data = res.data?.data?.data
-        if (data?.length > 0) {
-          const newData = data?.map(item => ({ ...item, fixedPrice: item.price }));
-          setClientPaymentsList(newData);
-          alertSuccessGetData();
-        }
-
-      } else {
-        alertFaultGetData();
-      }
-    },
-    []
-  );
-
-
-
-
-  const SendCustomerLedgerToMailApi = useCallback(
-    async () => {
-      const res = await callApi(
-        EHttpMethod.POST,
-        `/v1/erp-service/reports/send-customer-ledger-to-mail`,
-        {
-          // clientId: customer?.id,
-          startDate: fromDate,
-          endDate: toDate,
-          isExtended: isExtended,
-          mail: selectedContactById?.mail
-        }
-      );
-      if (res?.success) {
-        alertSuccess("reports.sendEmailSuccess")
-        onClickCloseEmailModal()
-      } else {
-        alertFault("reports.sendEmailFailed");
-      }
-    },
-    [fromDate, toDate, isExtended, selectedContactById]
-  );
-
   return {
     onSelectDeliveryTimeDates,
-    onClickCreateNewTransaction,
-    onClickSendingTicketByEmail,
+    handleDocumentTypeChange,
     onClickPrintCard,
     onClickShowCard,
-    onClickAdjustments,
-    onChangeIsExtended,
     getTableDataRows,
-    onClickCloseEmailModal,
-    setSelectedContactById,
-    onChangeUpdateClientContact,
-    onClickCloseAdjustmentsModal,
-    getClientPaymentItems,
-    SendCustomerLedgerToMailApi,
-    onClickCloseCreateTransactionModal,
-    handleDocumentTypeChange,
     documentType,
-    documentsTypeList,
-    isopenCreateTransactionModal,
-    isExtended,
-    showTable,
     resetDatePicker,
+    documentsTypeList,
+    showTable,
     tableHeaders,
-    isopenEmailModal,
-    selectedContactById,
-    isopenAdjustmentsModal,
-    clientPaymentsList,
     dataTable
 
   };
