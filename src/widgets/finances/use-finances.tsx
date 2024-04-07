ccaccd9165d98229bb0e93eba1c09353912048a3
lamaAnbtawe <106581571+lamaAnbtawe@users.noreply.github.com>
2024-04-07 16:39:41 +0300

    Update use-finances.tsx

--- src/widgets/finances/use-finances.tsx
+++ src/widgets/finances/use-finances.tsx
@@ -12,16 +12,14 @@ type Account = {
 
 const useFinances = () => {
   const { callApi } = useGomakeAxios();
-  const { alertFaultUpdate , alertSuccessUpdate, alertFaultGetData } = useSnackBar();
+  const { alertFaultUpdate, alertSuccessUpdate, alertFaultGetData } = useSnackBar();
   const [financeState, setFinanceState] = useState<any>();
   const [accountList, setAccountList] = useState<Account[]>([]);
-
-
   const [accountName, setAccountName] = useState<string>("")
   const [accountEmail, setAccountEmail] = useState<string>("")
   const [dayOfMonth, setDayOfMonth] = useState<string>("")
 
-  const onChangeAccountName = (v : any) => {
+  const onChangeAccountName = (v: any) => {
     setAccountName(v);
   }
   const onResetAccountName = () => {
@@ -67,50 +65,50 @@ const useFinances = () => {
       account.accountCode,
       account.accountName,
       <UpdatedTextInput
-      key={index}
-      onInputChange={(newValue) => {
-        const updatedList = [...accountList];
-        updatedList[index] = {
-          ...updatedList[index],
-          cpaAccountCode: newValue
-        };
-        setAccountList(updatedList);
-      }}
-      onCancel={() => {
-        // Reset to the previous value of cpaAccountCode
-        const previousValue = pureAccountList[index].cpaAccountCode;
-        const updatedList = [...pureAccountList];
-        updatedList[index] = {
-          ...updatedList[index],
-          cpaAccountCode: previousValue
-        };
-        setAccountList(updatedList);
-      }}
-      onUpdate={() => {
-        onClickUpdateCpaAccountCode(account.accountCode, account.cpaAccountCode);
-      }}
-      value={account.cpaAccountCode}
-      height="30px"
-      width="120px"
-      withBorder={true}
-    />
+        key={index}
+        onInputChange={(newValue) => {
+          const updatedList = [...accountList];
+          updatedList[index] = {
+            ...updatedList[index],
+            cpaAccountCode: newValue
+          };
+          setAccountList(updatedList);
+        }}
+        onCancel={() => {
+          const previousValue = pureAccountList[index].cpaAccountCode;
+         // const updatedList = [...pureAccountList];
+          const updatedList = [...accountList];
+          updatedList[index] = {
+            ...updatedList[index],
+            cpaAccountCode: previousValue
+          };
+          setAccountList(updatedList);
+        }}
+        onUpdate={() => {
+          onClickUpdateCpaAccountCode(account.accountCode, account.cpaAccountCode);
+        }}
+        value={account.cpaAccountCode}
+        height="30px"
+        width="120px"
+        withBorder={true}
+      />
     ]);
   }, [accountList]);
-  
+
   const onClickUpdateCpaMangerName = async () => {
     const callBack = (res) => {
       if (res.success) {
         setFinanceState(prevState => ({
           ...prevState,
           cpaName: accountName // Update cpaName with accountName
-      }));
+        }));
         alertSuccessUpdate();
       }
       else {
         alertFaultUpdate();
       }
     };
-    await updateCpaManagerNameApi(callApi, callBack, { mangerName: accountName});
+    await updateCpaManagerNameApi(callApi, callBack, { mangerName: accountName });
   };
 
   const onClickUpdateCpaMangerMail = async () => {
@@ -120,13 +118,13 @@ const useFinances = () => {
         setFinanceState(prevState => ({
           ...prevState,
           cpaMail: accountEmail
-      }));
+        }));
       }
       else {
         alertFaultUpdate();
       }
     };
-    await updateCpaManagerMailApi(callApi, callBack, { mangerMail: accountEmail});
+    await updateCpaManagerMailApi(callApi, callBack, { mangerMail: accountEmail });
   };
 
   const onClickUpdateDayInMonth = async () => {
@@ -135,21 +133,35 @@ const useFinances = () => {
         alertSuccessUpdate();
         setFinanceState(prevState => ({
           ...prevState,
-          cpaTransmitDate: dayOfMonth 
-      }));
+          cpaTransmitDate: dayOfMonth
+        }));
       }
       else {
         alertFaultUpdate();
       }
     };
-    await updateSendCpaReportApi(callApi, callBack, { dayInMonth: dayOfMonth});
+    await updateSendCpaReportApi(callApi, callBack, { dayInMonth: dayOfMonth });
   };
 
-  
-  const onClickUpdateCpaAccountCode = async (accountCode , cpaAccountCode ) => {
+  const onClickUpdateCpaAccountCode = async (accountCode, cpaAccountCode) => {
     const callBack = (res) => {
       if (res.success) {
         alertSuccessUpdate();
+        const updatedFinanceAccounts = financeState?.accounts.map(account => {
+          if (account.accountCode === accountCode) {
+            return {
+              ...account,
+              cpaAccountCode: cpaAccountCode
+            };
+          }
+          return account;
+        });
+        setAccountList(updatedFinanceAccounts);
+        setPureAccountList(updatedFinanceAccounts);
+        setFinanceState(prevState => ({
+          ...prevState,
+          accounts: updatedFinanceAccounts
+        }));
       }
       else {
         alertFaultUpdate();
@@ -179,7 +191,7 @@ const useFinances = () => {
     getAccountRows,
     onResetDayOfMonth,
     onClickUpdateDayInMonth
-    
+
   };
 };
 
