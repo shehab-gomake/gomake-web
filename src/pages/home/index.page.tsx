589781aa9444fcb506ec7becde42b66a219ed368
riadFadila <126657900+riadFadila@users.noreply.github.com>
2024-05-05 15:20:11 +0300

    fix build -- add def-messanger interface to global

--- src/pages/home/index.page.tsx
+++ src/pages/home/index.page.tsx
@@ -1,6 +1,7 @@
 import {CustomerAuthLayout} from "@/layouts";
 import {HomePageComponentForAdmin} from "@/pages-components/admin/home/home";
-import {useEffect, useState} from "react";
+import {useState} from "react";
+import {IconButton} from "@mui/material";
 
 export default function Home() {
     return (
@@ -13,23 +14,22 @@ export default function Home() {
 export const CustomChatButton = () => {
     const [isOpen, setIsOpen] = useState(false);
 
-    const toggleChat = (e) => {
-        e.stopPropagation();
+    const toggleChat = () => {
         setIsOpen(!isOpen);
     };
     return (
         <>
-            <button onClick={toggleChat} style={{ position: 'fixed', bottom: '20px', right: '20px', zIndex: 1000, border: 0 }}>
+            <IconButton onClick={toggleChat} style={{ position: 'fixed', bottom: '20px', right: '20px', zIndex: 1000, border: 0 }}>
                 <img src="/Go-01.svg" alt="Chat" style={{ width: '50px', height: '50px', }} />
-            </button>
+            </IconButton>
             {
-              isOpen &&    <df-messenger
+              isOpen && <df-messenger
                     location="europe-west3"
-                    project-id="extended-legend-421710"
+                    projectId="extended-legend-421710"
                     agent-id="6e4422f6-82ba-4960-99d7-0b4685406eae"
                     language-code="en"
                     max-query-length="-1"
-                    style={{ display: 'block', position: 'fixed', bottom: '80px', right: '16px', zIndex: 999 }}>
+                    style={{ display: 'block', position: 'fixed' as 'fixed', bottom: '80px', right: '16px', zIndex: 999, height: '500px', width: '300px' }}>
                     <df-messenger-chat chat-title="go bot"/>
                 </df-messenger>
             }
