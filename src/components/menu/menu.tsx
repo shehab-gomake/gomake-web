6d27f7b760c3f2edc35d0c28073f660ad0841f24
lamaAnbtawe <106581571+lamaAnbtawe@users.noreply.github.com>
2024-05-07 15:54:58 +0300

    new design for sales filres

--- src/components/menu/menu.tsx
+++ src/components/menu/menu.tsx
@@ -1,17 +1,49 @@
-import { Menu } from "@mui/material";
-import * as React from "react";
+import { Menu } from '@mui/material';
+import { useTranslation } from 'react-i18next';
+
+// const GoMakeMenu = ({ handleClose, open, anchorEl, ...props }) => {
+//   const { children }: any = props;
+//   return (
+//     <Menu
+//           aria-labelledby="demo-positioned-button"
+//           anchorEl={anchorEl}
+//           open={open}
+//           onClose={handleClose}
+//           style={props.style}
+//         >
+//           {children}
+//     </Menu>
+//   );
+// };
+
+// export { GoMakeMenu };
+
+
 
 const GoMakeMenu = ({ handleClose, open, anchorEl, ...props }) => {
   const { children }: any = props;
+  const { t } = useTranslation();
+  const direction = t('direction');
+
   return (
     <Menu
-          aria-labelledby="demo-positioned-button"
-          anchorEl={anchorEl}
-          open={open}
-          onClose={handleClose}
-          style={props.style}
-        >
-          {children}
+      anchorReference="anchorEl"
+      anchorEl={anchorEl}
+      open={open}
+      onClose={handleClose}
+      anchorOrigin={
+        direction === 'rtl'
+          ? { vertical: 'bottom', horizontal: 'left' }
+          : { vertical: 'bottom', horizontal: 'right' }
+      }
+      transformOrigin={
+        direction === 'rtl'
+          ? { vertical: 'top', horizontal: 'left' }
+          : { vertical: 'top', horizontal: 'right' }
+      }
+      style={props.style}
+    >
+      {children}
     </Menu>
   );
 };
