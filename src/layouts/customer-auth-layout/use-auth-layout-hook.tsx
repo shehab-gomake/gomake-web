fb5e4ab5a1e948809346b821d5d7659754431aa9
lamaAnbtawe <106581571+lamaAnbtawe@users.noreply.github.com>
2024-05-23 18:44:34 +0300

    new changes

--- src/layouts/customer-auth-layout/use-auth-layout-hook.tsx
+++ src/layouts/customer-auth-layout/use-auth-layout-hook.tsx
@@ -8,6 +8,7 @@ import { Permissions } from "@/components/CheckPermission/enum";
 import LocalPrintshopOutlinedIcon from "@mui/icons-material/LocalPrintshopOutlined";
 import PendingActionsOutlinedIcon from "@mui/icons-material/PendingActionsOutlined";
 import { BankingIcon } from "@/icons/banking-icon";
+import { MachineIcon } from "@/icons/machine-icon";
 
 const useAuthLayoutHook = (permissionEnumValue?: Permissions, allowAnonymous?: boolean) => {
   const { isAuth } = useGomakeAuth(permissionEnumValue, allowAnonymous);
@@ -303,7 +304,7 @@ const useAuthLayoutHook = (permissionEnumValue?: Permissions, allowAnonymous?: b
         Permission: Permissions.SHOW_MACHINES,
         isList: false,
         icon: () => {
-          return <LocalPrintshopOutlinedIcon style={{ color: "#FFFFFF" }} />;
+          return <MachineIcon/>;
         },
         isProduction: true,
         tourData: 'menuMachines'
@@ -317,8 +318,7 @@ const useAuthLayoutHook = (permissionEnumValue?: Permissions, allowAnonymous?: b
         Permission: Permissions.MACHINES_ADMIN,
         isList: false,
         icon: () => {
-          return <LocalPrintshopOutlinedIcon style={{ color: "#FFFFFF" }} />;
-        },
+          return <MachineIcon/>;        },
         isProduction: true,
       },
       {
