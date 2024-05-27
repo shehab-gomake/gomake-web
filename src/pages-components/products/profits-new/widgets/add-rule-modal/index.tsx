7080530bebfe8dc472074afa29ca5f71d0e79c6c
shehab-gomake <129649590+shehab-gomake@users.noreply.github.com>
2024-05-27 14:01:16 +0300

    Link Condition in the Rule Modal

--- src/pages-components/products/profits-new/widgets/add-rule-modal/index.tsx
+++ src/pages-components/products/profits-new/widgets/add-rule-modal/index.tsx
@@ -389,7 +389,7 @@ const AddRuleModal = ({
                       placeholder={t("properties.condtion")}
                       value={rule.linkCondition}
                       onChange={(event, value) => {
-                        handleChange(index, "linkCondition", value);
+                        handleChange(index - 1, "linkCondition", value);
                       }}
                     />
                   </div>
