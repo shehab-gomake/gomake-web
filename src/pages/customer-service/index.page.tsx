3c853a1f526c485da3536977c03b61cee3b180ce
shehab-gomake <129649590+shehab-gomake@users.noreply.github.com>
2024-06-25 17:01:40 +0300

    add new page

---
+++ src/pages/customer-service/index.page.tsx
@@ -0,0 +1,11 @@
+import { CustomerAuthLayout } from "@/layouts";
+import { ActionPageWidget } from "@/pages-components/actions/actions";
+
+export default function CustomerServicePage() {
+  return (
+    <CustomerAuthLayout >
+      ddddd
+      {/* <ActionPageWidget /> */}
+    </CustomerAuthLayout>
+  );
+}
