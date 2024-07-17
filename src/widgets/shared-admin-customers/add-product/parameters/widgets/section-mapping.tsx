c77f8be0badace1091e00d26fbf2ed32151f7b2f
lamaAnbtawe <106581571+lamaAnbtawe@users.noreply.github.com>
2024-07-17 11:58:15 +0300

    Update section-mapping.tsx

--- src/widgets/shared-admin-customers/add-product/parameters/widgets/section-mapping.tsx
+++ src/widgets/shared-admin-customers/add-product/parameters/widgets/section-mapping.tsx
@@ -6,10 +6,12 @@ import {
   RequiredIcon,
   SettingIcon,
 } from "../../icons";
+import { Tooltip } from "@mui/material";
 import { LockedIcon } from "../../icons/lock-icon";
 import { NotLockedIcon } from "../../icons/not-lock-icon";
-import { NotDetailIcon } from "../../icons/not-detail-icon";
 import { DetailIcon } from "../../icons/detail-icon";
+import { NotDetailIcon } from "../../icons/not-detail-icon";
+import { useTranslation } from "react-i18next";
 
 const SectionMappingWidget = ({
   index,
@@ -24,158 +26,84 @@ const SectionMappingWidget = ({
   updatedProductParameteLocked,
   updatedProductParameterShowToClient
 }) => {
+  const { t } = useTranslation();
+  const renderIconWithTooltip = (condition, trueIcon, falseIcon, trueTitle, falseTitle, onClickHandler) => (
+    <Tooltip title={condition ? t(`tooltips.${trueTitle}`) : t(`tooltips.${falseTitle}`)} placement="top">
+      <div style={{ cursor: "pointer" }} onClick={onClickHandler}>
+        {condition ? trueIcon : falseIcon}
+      </div>
+    </Tooltip>
+  );
+
   return (
     <div key={index} style={clasess.subSectionContainer}>
       <div style={clasess.subSectionTitleStyle}>{subSection.name}</div>
       <div style={clasess.parametersContainer}>
         {subSection?.parameters
           ?.filter((x) => !x.isHiddenInSetting)
-          ?.map((parameter, index) => {
-            console.log("parameter", parameter)
+          ?.map((parameter, paramIndex) => {
+
             const isHiddenIcon = relatedParameters?.some(
               (relatedParam) => relatedParam.parameterId === parameter.id
             );
+
             return (
-              <div key={index}>
+              <div key={paramIndex}>
                 <div style={clasess.parameterContainer}>
                   <div style={clasess.parameterLabelStyle}>
-                    <div style={clasess.paramNameStyle}>{parameter?.name} <small>&nbsp;{parameter?.defaultUnit}</small></div>
+                    <div style={clasess.paramNameStyle}>
+                      {parameter?.name} <small>&nbsp;{parameter?.defaultUnit}</small>
+                    </div>
+                    {parameter?.parameterType === EParameterTypes.SELECT_CHILDS_PARAMETERS && (
+                      <div
+                        style={clasess.plusIconStyle}
+                        onClick={() =>
+                          onOpenModal(parameter, section?.id, subSection?.id)
+                        }
+                      >
+                        <SettingIcon width={20} height={20} />
+                      </div>
+                    )}
 
-                    {parameter?.parameterType ===
-                      EParameterTypes.SELECT_CHILDS_PARAMETERS && (
-                        <div
-                          style={clasess.plusIconStyle}
-                          onClick={() =>
-                            onOpenModal(parameter, section?.id, subSection?.id)
-                          }
-                        >
-                          <SettingIcon width={20} height={20} />
-                        </div>
-                      )}
                     {!isHiddenIcon && (
-                      <>
-                        {parameter?.isHidden ? (
-                          <div
-                            style={{ cursor: "pointer" }}
-                            onClick={() =>
-                              updatedProductParameterHidden(
-                                section?.id,
-                                subSection?.id,
-                                parameter
-                              )
-                            }
-                          >
-                            <HiddenIcon />
-                          </div>
-                        ) : (
-                          <div
-                            style={{ cursor: "pointer" }}
-                            onClick={() =>
-                              updatedProductParameterHidden(
-                                section?.id,
-                                subSection?.id,
-                                parameter
-                              )
-                            }
-                          >
-                            <NotHiddenIcon />
-                          </div>
-                        )}
-                      </>
-                    )}
-
-                    {parameter?.parameterType !== EParameterTypes.SWITCH ? (
-                      parameter?.isRequired ? (
-                        <div
-                          style={{ cursor: "pointer" }}
-                          onClick={() =>
-                            updatedProductParameteRequierd(
-                              section?.id,
-                              subSection?.id,
-                              parameter
-                            )
-                          }
-                        >
-                          <RequiredIcon />
-                        </div>
-                      ) : (
-                        <div
-                          style={{ cursor: "pointer" }}
-                          onClick={() =>
-                            updatedProductParameteRequierd(
-                              section?.id,
-                              subSection?.id,
-                              parameter
-                            )
-                          }
-                        >
-                          <NotRequiredIcon />
-                        </div>
-                      )
-                    ) : (
-                      <></>
-                    )}
-                    {(
-                      parameter?.isLock ? (
-                        <div
-                          style={{ cursor: "pointer" }}
-                          onClick={() =>
-                            updatedProductParameteLocked(
-                              section?.id,
-                              subSection?.id,
-                              parameter
-                            )
-                          }
-                        >
-                          <LockedIcon />
-                        </div>
-                      ) : (
-                        <div
-                          style={{ cursor: "pointer" }}
-                          onClick={() =>
-                            updatedProductParameteLocked(
-                              section?.id,
-                              subSection?.id,
-                              parameter
-                            )
-                          }
-                        >
-                          <NotLockedIcon />
-                        </div>
+                      renderIconWithTooltip(
+                        parameter?.isHidden,
+                        <HiddenIcon />,
+                        <NotHiddenIcon />,
+                        "hidden",
+                        "visible",
+                        () => updatedProductParameterHidden(section?.id, subSection?.id, parameter)
                       )
                     )}
 
-                    {
-                      <>
-                        {parameter?.isShowToClient ? (
-                          <div
-                            style={{ cursor: "pointer" }}
-                            onClick={() =>
-                              updatedProductParameterShowToClient(
-                                section?.id,
-                                subSection?.id,
-                                parameter
-                              )
-                            }
-                          >
-                            <DetailIcon />
-                          </div>
-                        ) : (
-                          <div
-                            style={{ cursor: "pointer" }}
-                            onClick={() =>
-                              updatedProductParameterShowToClient(
-                                section?.id,
-                                subSection?.id,
-                                parameter
-                              )
-                            }
-                          >
-                            <NotDetailIcon />
-                          </div>
-                        )}
-                      </>
-                    }
+                    {parameter?.parameterType !== EParameterTypes.SWITCH && (
+                      renderIconWithTooltip(
+                        parameter?.isRequired,
+                        <RequiredIcon />,
+                        <NotRequiredIcon />,
+                        "required",
+                        "notRequired",
+                        () => updatedProductParameteRequierd(section?.id, subSection?.id, parameter)
+                      )
+                    )}
+
+                    {renderIconWithTooltip(
+                      parameter?.isLock,
+                      <LockedIcon />,
+                      <NotLockedIcon />,
+                      "readOnly",
+                      "editable",
+                      () => updatedProductParameteLocked(section?.id, subSection?.id, parameter)
+                    )}
+
+                    {renderIconWithTooltip(
+                      parameter?.isShowToClient,
+                      <DetailIcon />,
+                      <NotDetailIcon />,
+                      "shownInDetails",
+                      "hiddenFromDetails",
+                      () => updatedProductParameterShowToClient(section?.id, subSection?.id, parameter)
+                    )}
                   </div>
                   <div style={clasess.renderParameterTypeContainer}>
                     {_renderParameterType(
@@ -183,7 +111,7 @@ const SectionMappingWidget = ({
                       subSection?.id,
                       parameter,
                       subSection?.parameters,
-                      index
+                      paramIndex
                     )}
                   </div>
                 </div>
